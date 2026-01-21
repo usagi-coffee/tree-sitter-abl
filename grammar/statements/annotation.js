7302a3a8ac665d646eda59473ffc74a37e0a27d8
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-01-21 20:02:19 +0100

    grammar: statements: implement annotations
    Fixes #103

--- grammar/statements/annotation.js
+++ grammar/statements/annotation.js
@@ -2,8 +2,17 @@ module.exports = () => ({
   annotation_statement: ($) =>
     seq(
       "@",
-      field("name", $.identifier),
-      optional(seq("(", optional($._expression_list), ")")),
+      field("name", $.annotation_name),
+      optional(seq("(", optional($.annotation_attribute_list), ")")),
       $._terminator,
     ),
+  annotation_attribute_list: ($) =>
+    seq($.annotation_attribute, repeat(seq(",", $.annotation_attribute))),
+  annotation_attribute: ($) =>
+    seq(
+      field("name", $.annotation_name),
+      "=",
+      field("value", $.string_literal),
+    ),
+  annotation_name: () => token(/[^0-9\s()=,\.][^\s()=,\.]*/),
 });
