b6ac1dc547828493f642c4aeeacf96c6c0b3b671
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-01-28 11:57:43 +0100

    grammar: connect fixes

--- grammar/statements/connect.js
+++ grammar/statements/connect.js
@@ -2,11 +2,31 @@ module.exports = ({ kw, tkw }) => ({
   connect_statement: ($) =>
     seq(
       tkw("CONNECT"),
-      choice(
-        seq(tkw("VALUE"), "(", field("value", $._expression), ")"),
-        field("database", $._expression),
+      optional(
+        field(
+          "database",
+          choice($.identifier, $.string_literal, $.function_call),
+        ),
       ),
+      repeat(alias($.__connect_option, $.connect_option)),
       optional(tkw("NO-ERROR")),
       $._terminator,
+    ),
+
+  __connect_database: ($) => choice($.__connect_option, $.string_literal),
+
+  __connect_option: ($) =>
+    seq(
+      "-",
+      field(
+        "parameter",
+        alias(token.immediate(/[\p{L}\p{N}_]+/u), $.identifier),
+      ),
+      optional(
+        field(
+          "value",
+          choice($.identifier, $.string_literal, $.number_literal),
+        ),
+      ),
     ),
 });
