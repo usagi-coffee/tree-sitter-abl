5c0b1f815561ac2e8545a0a0bffe1cc0585207d5
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-01-29 18:11:47 +0100

    grammar: choose modifiers

--- grammar/statements/choose.js
+++ grammar/statements/choose.js
@@ -18,15 +18,22 @@ module.exports = ({ kw, tkw }) => ({
           ),
         ),
       ),
-      optional(tkw("AUTO-RETURN")),
-      optional(seq(kw("COLOR"), field("color", $.color_phrase))),
-      optional(seq(tkw("GO-ON"), "(", repeat1($.identifier), ")")),
-      optional(seq(kw("KEYS"), field("keys", $.identifier))),
-      optional(tkw("NO-ERROR")),
-      optional(seq(kw("PAUSE"), field("pause", $._expression))),
+      repeat(
+        choice(
+          alias($.__choose_auto_return, $.auto_return),
+          seq(kw("COLOR"), field("color", $.color_phrase)),
+          alias($.__choose_go_on, $.go_on),
+          seq(kw("KEYS"), field("keys", $.identifier)),
+          alias($.__choose_no_error, $.no_error),
+          seq(kw("PAUSE"), field("pause", $._expression)),
+        ),
+      ),
       optional(alias($.__choose_frame_phrase, $.frame_phrase)),
     ),
   __choose_frame_phrase: ($) =>
     seq(kw("WITH"), kw("FRAME"), field("frame", $.identifier)),
+  __choose_auto_return: ($) => tkw("AUTO-RETURN"),
+  __choose_go_on: ($) => seq(tkw("GO-ON"), "(", repeat1($.identifier), ")"),
+  __choose_no_error: ($) => tkw("NO-ERROR"),
   __choose_color_value: ($) => $._expression,
 });
