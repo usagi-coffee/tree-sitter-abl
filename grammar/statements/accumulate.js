56f2937235de045ffac28b798d450d6b5e98017e
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-02-01 05:06:56 +0100

    grammar: accumulate optimimization

--- grammar/statements/accumulate.js
+++ grammar/statements/accumulate.js
@@ -1,11 +1,9 @@
 module.exports = ({ kw, tkw }) => ({
   accumulate_statement: ($) =>
-    seq(tkw("ACCUMULATE"), $.__accumulate_body, $._terminator),
-
-  __accumulate_body: ($) =>
     seq(
-      alias($.__accumulate_item, $.accumulate),
-      repeat(alias($.__accumulate_item, $.accumulate)),
+      tkw("ACCUMULATE"),
+      repeat1(alias($.__accumulate_item, $.accumulate)),
+      $._terminator
     ),
 
   __accumulate_item: ($) =>
