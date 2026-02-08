892db9d3dbb245874c489ef842a47bd6e588658e
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-02-08 14:11:40 +0100

    grammar: fixes

--- grammar/precedences/subscribe.js
+++ grammar/precedences/subscribe.js
@@ -3,4 +3,7 @@ module.exports = ($) => [
   // Purpose: prefer function call when subscribe expression uses '('.
   // Example: SUBSCRIBE TO myFunc() IN publisher.
   [$.function_call, $.__subscribe_expression],
+  // Purpose: when SUBSCRIBE starts with PROCEDURE/PROC-like token, prefer subscribe body parse.
+  // Example: SUBSCRIBE PROCEDURE hProc TO "evt" IN src.
+  [ $.__subscribe_body, $._identifier_or_qualified_name],
 ];
