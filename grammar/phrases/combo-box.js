a95be5de6c210cd51adf4b956075a8d2e2a472de
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-02-10 07:33:10 +0100

    grammar: optimize combo-box phrase

--- grammar/phrases/combo-box.js
+++ grammar/phrases/combo-box.js
@@ -3,15 +3,18 @@ module.exports = ({ kw }) => ({
     seq(
       field("widget", kw("COMBO-BOX")),
       optional(choice($.__combo_box_list_items, $.__combo_box_list_item_pairs)),
-      optional(seq(kw("INNER-LINES"), field("inner_lines", $.number_literal))),
-      optional($.size_phrase),
-      optional(kw("SORT")),
-      optional(seq(kw("TOOLTIP"), field("tooltip", $._expression))),
-      optional(choice(kw("SIMPLE"), kw("DROP-DOWN"), kw("DROP-DOWN-LIST"))),
-      optional(seq(kw("MAX-CHARS"), field("max_chars", $.number_literal))),
-      optional(seq(kw("AUTO-COMPLETION"), optional(kw("UNIQUE-MATCH")))),
+      repeat($.__combo_box_option),
     ),
-
+  __combo_box_option: ($) =>
+    choice(
+      seq(kw("INNER-LINES"), field("inner_lines", $.number_literal)),
+      $.size_phrase,
+      kw("SORT"),
+      seq(kw("TOOLTIP"), field("tooltip", $._expression)),
+      choice(kw("SIMPLE"), kw("DROP-DOWN"), kw("DROP-DOWN-LIST")),
+      seq(kw("MAX-CHARS"), field("max_chars", $.number_literal)),
+      seq(kw("AUTO-COMPLETION"), optional(kw("UNIQUE-MATCH"))),
+    ),
   __combo_box_list_items: ($) =>
     seq(kw("LIST-ITEMS"), field("items", $.__combo_box_item_list)),
   __combo_box_item_list: ($) =>
