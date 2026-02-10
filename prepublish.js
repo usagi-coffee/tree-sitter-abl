58bbd9baf41f0b3ea973831140e9eb4b77536bc7
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-02-10 05:01:35 +0100

    meta: check grammar.json and node-types.json in prepublish

--- prepublish.js
+++ prepublish.js
@@ -1,23 +1,30 @@
 import { $ } from "bun";
 
+async function check_skipped(src) {
+  const ls = await $`git ls-files -v ${src}`;
+  const [status = "", file = ""] = ls.text()?.split(" ");
+
+  if (file.trim() !== src) {
+    console.error(`${src} not found, aborting...`);
+    process.exit(1);
+  }
+
+  if (status.trim() !== "H") {
+    console.error(
+      `${src} is most likely skipped, undo worktree before deploying, aborting...`
+    );
+
+    process.exit(1);
+  }
+}
+
+await check_skipped("src/parser.c");
+await check_skipped("src/grammar.json");
+await check_skipped("src/node-types.json");
+
 const { exitCode } = await $`npm whoami`;
 if (exitCode !== 0) {
   console.error("Authenticate on npm before publishing");
-  process.exit(1);
-}
-
-const ls = await $`git ls-files -v src/parser.c`;
-const [status = "", file = ""] = ls.text()?.split(" ");
-
-if (file.trim() !== "src/parser.c") {
-  console.error("src/parser.c not found, aborting...");
-  process.exit(1);
-}
-
-if (status.trim() !== "H") {
-  console.error(
-    "src/parser.c is most likely skipped, undo worktree before deploying, aborting...",
-  );
   process.exit(1);
 }
 
