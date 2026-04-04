942db24e2869fe78886e4066be40d64372ef0468
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-05 00:39:12 +0200

    grammar: share __on_phrase_return and __undo_lnr_target in on-error

--- grammar/phrases/on-error.js
+++ grammar/phrases/on-error.js
@@ -12,21 +12,7 @@ module.exports = ({ kw }) => ({
   __on_error_action: ($) =>
     choice(
       seq(kw("THROW"), optional(field("throw_value", $._expression))),
-      seq(kw("LEAVE"), optional(field("leave_label", $.identifier))),
-      seq(kw("NEXT"), optional(field("next_label", $.identifier))),
-      seq(kw("RETRY"), optional(field("retry_label", $.identifier))),
-      $.__on_error_return,
-    ),
-
-  __on_error_return: ($) =>
-    seq(
-      kw("RETURN"),
-      optional(
-        choice(
-          seq(kw("ERROR"), optional(field("error_value", $._expression))),
-          kw("NO-APPLY"),
-          field("return_value", $._expression),
-        ),
-      ),
+      $.__undo_lnr_target,
+      $.__on_phrase_return,
     ),
 });
