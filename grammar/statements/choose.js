36b08fb528f727537d6db400d1a4cc037281d70f
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-05 15:49:04 +0200

    grammar: share __go_on_phrase in choose.js instead of inline duplicate

--- grammar/statements/choose.js
+++ grammar/statements/choose.js
@@ -11,7 +11,7 @@ module.exports = ({ kw }) => ({
         choice(
           alias(kw("AUTO-RETURN"), $.auto_return),
           seq(kw("COLOR"), field("color", $.color_phrase)),
-          alias($.__choose_go_on, $.go_on),
+          alias($.__go_on_phrase, $.go_on),
           seq(kw("KEYS"), field("keys", $.identifier)),
           alias(kw("NO-ERROR"), $.no_error),
           seq(kw("PAUSE"), field("pause", $._expression)),
@@ -23,20 +23,5 @@ module.exports = ({ kw }) => ({
     seq(
       field("field", $._identifier_or_qualified_name),
       optional(seq(kw("HELP"), field("help", $.string_literal))),
-    ),
-
-  __choose_go_on: ($) =>
-    seq(
-      kw("GO-ON"),
-      "(",
-      choice(
-        $.identifier,
-        $.string_literal,
-        seq(
-          choice($.identifier, $.string_literal),
-          repeat(seq(optional(","), choice($.identifier, $.string_literal))),
-        ),
-      ),
-      ")",
     ),
 });
