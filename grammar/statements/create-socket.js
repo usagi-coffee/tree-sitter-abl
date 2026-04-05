f4290f2034350346e553543df7602c26e28904da
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-05 16:06:33 +0200

    grammar: unify socket type in create-socket

--- grammar/statements/create-socket.js
+++ grammar/statements/create-socket.js
@@ -2,8 +2,9 @@ module.exports = ({ kw }) => ({
   create_socket_statement: ($) => seq(kw("CREATE"), $.__create_socket_body),
 
   __create_socket_body: ($) =>
-    choice(
-      seq(kw("SOCKET"), field("name", $.identifier), $._no_error_terminator),
-      seq(kw("SERVER-SOCKET"), field("name", $.identifier), $._no_error_terminator),
+    seq(
+      choice(kw("SOCKET"), kw("SERVER-SOCKET")),
+      field("name", $.identifier),
+      $._no_error_terminator
     ),
 });
