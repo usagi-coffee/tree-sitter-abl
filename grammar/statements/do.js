106ff29e3f938a1ef0e86c010caadb26e96b5061
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-18 23:14:12 +0200

    grammar: share do block tail

--- grammar/statements/do.js
+++ grammar/statements/do.js
@@ -13,23 +13,20 @@ module.exports = ({ kw }) => ({
     choice(
       seq(alias(kw("TRANSACTION"), $.transaction), optional($.__do_body_after_first_transaction)),
       seq($.__do_condition_or_loop_phrase, optional($.__do_body_after_condition_or_loop)),
-      seq(repeat1($.__do_block_option), $.body),
-      $.body,
+      $.__do_block_tail,
     ),
   __do_body_after_first_transaction: ($) =>
     choice(
       seq($.__do_condition_or_loop_phrase, optional($.__do_body_after_condition_or_loop)),
-      seq(repeat1($.__do_block_option), $.body),
-      $.body,
+      $.__do_block_tail,
     ),
   __do_body_after_condition_or_loop: ($) =>
     choice(
       seq(alias(kw("TRANSACTION"), $.transaction), optional($.__do_body_after_second_transaction)),
-      seq(repeat1($.__do_block_option), $.body),
-      $.body,
+      $.__do_block_tail,
     ),
-  __do_body_after_second_transaction: ($) =>
-    choice(seq(repeat1($.__do_block_option), $.body), $.body),
+  __do_body_after_second_transaction: ($) => $.__do_block_tail,
+  __do_block_tail: ($) => choice(seq(repeat1($.__do_block_option), $.body), $.body),
   __do_block_option: ($) =>
     choice(
       $.stop_after_phrase,
