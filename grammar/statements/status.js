d92c556a9429fc52f5cea47347fd1985a8502687
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-19 01:44:58 +0200

    grammar: inline status mode branches

--- grammar/statements/status.js
+++ grammar/statements/status.js
@@ -2,10 +2,11 @@ module.exports = ({ kw }) => ({
   status_statement: ($) => seq(kw("STATUS"), $.__status_body, $._terminator),
 
   __status_body: ($) =>
-    seq(choice($.__status_default_body, $.__status_input_body), optional($.in_window_phrase)),
-
-  __status_default_body: ($) => seq(kw("DEFAULT"), optional(field("message", $._expression))),
-
-  __status_input_body: ($) =>
-    seq(kw("INPUT"), optional(choice(kw("OFF"), field("message", $._expression)))),
+    seq(
+      choice(
+        seq(kw("DEFAULT"), optional(field("message", $._expression))),
+        seq(kw("INPUT"), optional(choice(kw("OFF"), field("message", $._expression)))),
+      ),
+      optional($.in_window_phrase),
+    ),
 });
