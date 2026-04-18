71af50e004ba8fd1ceef34e95ab3f04018e8189a
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-19 00:24:17 +0200

    grammar: inline scroll from-current tail

--- grammar/statements/scroll.js
+++ grammar/statements/scroll.js
@@ -2,11 +2,12 @@ module.exports = ({ kw }) => ({
   scroll_statement: ($) => seq(kw("SCROLL"), optional($.__scroll_tail), $._terminator),
   __scroll_tail: ($) =>
     choice(
-      seq(alias(kw("FROM-CURRENT"), $.from_current), optional($.__scroll_tail_after_from_current)),
+      seq(
+        alias(kw("FROM-CURRENT"), $.from_current),
+        optional(choice(seq($.__scroll_direction, optional($.frame_phrase)), $.frame_phrase)),
+      ),
       seq($.__scroll_direction, optional($.frame_phrase)),
       $.frame_phrase,
     ),
-  __scroll_tail_after_from_current: ($) =>
-    choice(seq($.__scroll_direction, optional($.frame_phrase)), $.frame_phrase),
   __scroll_direction: ($) => choice(alias(kw("UP"), $.up), alias(kw("DOWN"), $.down)),
 });
