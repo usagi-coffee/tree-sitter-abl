1af1b7a6be0ecefc71d8d7dd01d426475b6b2bba
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-18 22:54:32 +0200

    grammar: share hide window tail

--- grammar/statements/hide.js
+++ grammar/statements/hide.js
@@ -4,20 +4,16 @@ module.exports = ({ kw }) => ({
     choice(
       seq($._stream_phrase, optional($.__hide_tail_after_stream)),
       seq($.__hide_target, optional($.__hide_tail_after_target)),
-      seq(alias(kw("NO-PAUSE"), $.no_pause), optional($.in_window_phrase)),
-      $.in_window_phrase,
+      $.__hide_window_tail,
     ),
   __hide_tail_after_stream: ($) =>
     choice(
       seq($.__hide_target, optional($.__hide_tail_after_target)),
-      seq(alias(kw("NO-PAUSE"), $.no_pause), optional($.in_window_phrase)),
-      $.in_window_phrase,
+      $.__hide_window_tail,
     ),
-  __hide_tail_after_target: ($) =>
-    choice(
-      seq(alias(kw("NO-PAUSE"), $.no_pause), optional($.in_window_phrase)),
-      $.in_window_phrase,
-    ),
+  __hide_tail_after_target: ($) => $.__hide_window_tail,
+  __hide_window_tail: ($) =>
+    choice(seq(alias(kw("NO-PAUSE"), $.no_pause), optional($.in_window_phrase)), $.in_window_phrase),
   __hide_target: ($) =>
     choice(alias(kw("MESSAGE"), $.message), alias(kw("ALL"), $.all), $.widget_phrase),
 });
