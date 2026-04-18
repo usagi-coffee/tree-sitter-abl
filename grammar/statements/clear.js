910d672f2de37978189d61966b4799a2ac0d8bd7
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-18 23:21:54 +0200

    grammar: share clear no-pause tail

--- grammar/statements/clear.js
+++ grammar/statements/clear.js
@@ -3,10 +3,10 @@ module.exports = ({ kw }) => ({
   __clear_tail: ($) =>
     choice(
       seq($.__clear_frame, optional($.__clear_tail_after_frame)),
-      seq(alias(kw("ALL"), $.all), optional(alias(kw("NO-PAUSE"), $.no_pause))),
-      alias(kw("NO-PAUSE"), $.no_pause),
+      $.__clear_all_no_pause_tail,
     ),
-  __clear_tail_after_frame: ($) =>
+  __clear_tail_after_frame: ($) => $.__clear_all_no_pause_tail,
+  __clear_all_no_pause_tail: ($) =>
     choice(
       seq(alias(kw("ALL"), $.all), optional(alias(kw("NO-PAUSE"), $.no_pause))),
       alias(kw("NO-PAUSE"), $.no_pause),
