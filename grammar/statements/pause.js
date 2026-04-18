8eb38000345188529149e498ed23f57606232eb0
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-18 22:56:38 +0200

    grammar: share pause message window tail

--- grammar/statements/pause.js
+++ grammar/statements/pause.js
@@ -4,16 +4,15 @@ module.exports = ({ kw }) => ({
     choice(
       seq(field("duration", $._expression), optional($.__pause_tail_after_duration)),
       seq(alias(kw("BEFORE-HIDE"), $.before_hide), optional($.__pause_tail_after_before_hide)),
-      seq($.__pause_message, optional($.in_window_phrase)),
-      $.in_window_phrase,
+      $.__pause_message_window_tail,
     ),
   __pause_tail_after_duration: ($) =>
     choice(
       seq(alias(kw("BEFORE-HIDE"), $.before_hide), optional($.__pause_tail_after_before_hide)),
-      seq($.__pause_message, optional($.in_window_phrase)),
-      $.in_window_phrase,
+      $.__pause_message_window_tail,
     ),
-  __pause_tail_after_before_hide: ($) =>
+  __pause_tail_after_before_hide: ($) => $.__pause_message_window_tail,
+  __pause_message_window_tail: ($) =>
     choice(seq($.__pause_message, optional($.in_window_phrase)), $.in_window_phrase),
   __pause_message: ($) =>
     choice(
