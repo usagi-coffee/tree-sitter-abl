d080289bdd8993bf4fbefd55f40f949baec8e029
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-04-18 23:40:46 +0200

    grammar: optimize clear tail reuse

--- grammar/statements/clear.js
+++ grammar/statements/clear.js
@@ -2,10 +2,9 @@ module.exports = ({ kw }) => ({
   clear_statement: ($) => seq(kw("CLEAR"), optional($.__clear_tail), $._terminator),
   __clear_tail: ($) =>
     choice(
-      seq($.__clear_frame, optional($.__clear_tail_after_frame)),
+      seq($.__clear_frame, optional($.__clear_all_no_pause_tail)),
       $.__clear_all_no_pause_tail,
     ),
-  __clear_tail_after_frame: ($) => $.__clear_all_no_pause_tail,
   __clear_all_no_pause_tail: ($) =>
     choice(
       seq(alias(kw("ALL"), $.all), optional(alias(kw("NO-PAUSE"), $.no_pause))),
