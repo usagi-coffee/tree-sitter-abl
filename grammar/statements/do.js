9151bc65b5e72de4899b6eaaea9685fa95d40946
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-19 01:55:10 +0200

    grammar: remove do transaction tail passthrough
    ACTION_COUNT: 54943 -> 54941 (-2)
    STATE_COUNT: 38464 -> 38463 (-1)
    LARGE_STATE_COUNT: 17088 -> 17088 (0)

--- grammar/statements/do.js
+++ grammar/statements/do.js
@@ -22,10 +22,9 @@ module.exports = ({ kw }) => ({
     ),
   __do_body_after_condition_or_loop: ($) =>
     choice(
-      seq(alias(kw("TRANSACTION"), $.transaction), optional($.__do_body_after_second_transaction)),
+      seq(alias(kw("TRANSACTION"), $.transaction), optional($.__do_block_tail)),
       $.__do_block_tail,
     ),
-  __do_body_after_second_transaction: ($) => $.__do_block_tail,
   __do_block_tail: ($) => choice(seq(repeat1($.__do_block_option), $.body), $.body),
   __do_block_option: ($) =>
     choice(
