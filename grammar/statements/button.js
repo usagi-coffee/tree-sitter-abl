706379d2d8597edb292646ea96df12b2a3795307
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-19 01:30:55 +0200

    grammar: inline button options
    ACTION_COUNT: 54973 -> 54969 (-4)
    STATE_COUNT: 38476 -> 38475 (-1)
    LARGE_STATE_COUNT: 17088 -> 17088 (0)

--- grammar/statements/button.js
+++ grammar/statements/button.js
@@ -8,31 +8,37 @@ module.exports = ({ kw }) => ({
       $._terminator,
     ),
 
-  __button_body: ($) => seq(field("name", $.identifier), repeat($.__button_option)),
-
-  __button_option: ($) =>
-    choice(
-      alias(kw("AUTO-GO"), $.auto_go),
-      alias(kw("AUTO-ENDKEY"), $.auto_endkey),
-      alias(kw("DEFAULT"), $.default),
-      $._color_font_option,
-      seq(kw("CONTEXT-HELP-ID"), field("context_help_id", $._expression)),
-      alias(kw("DROP-TARGET"), $.drop_target),
-      seq(kw("IMAGE-DOWN"), field("image_down", $.image_phrase)),
-      seq(kw("IMAGE"), field("image", $.image_phrase)),
-      seq(kw("IMAGE-UP"), field("image_up", $.image_phrase)),
-      seq(kw("IMAGE-INSENSITIVE"), field("image_insensitive", $.image_phrase)),
-      seq(kw("MOUSE-POINTER"), field("mouse_pointer", $._expression)),
-      seq(kw("LABEL"), field("label", $._identifier_or_string_literal)),
-      seq(kw("LIKE"), field("like", $.identifier)),
-      seq(
-        choice(kw("SIZE"), kw("SIZE-CHARS"), kw("SIZE-PIXELS")),
-        field("width", $._expression),
-        kw("BY"),
-        field("height", $._expression),
+  __button_body: ($) =>
+    seq(
+      field("name", $.identifier),
+      repeat(
+        choice(
+          alias(kw("AUTO-GO"), $.auto_go),
+          alias(kw("AUTO-ENDKEY"), $.auto_endkey),
+          alias(kw("DEFAULT"), $.default),
+          $._color_font_option,
+          seq(kw("CONTEXT-HELP-ID"), field("context_help_id", $._expression)),
+          alias(kw("DROP-TARGET"), $.drop_target),
+          seq(kw("IMAGE-DOWN"), field("image_down", $.image_phrase)),
+          seq(kw("IMAGE"), field("image", $.image_phrase)),
+          seq(kw("IMAGE-UP"), field("image_up", $.image_phrase)),
+          seq(kw("IMAGE-INSENSITIVE"), field("image_insensitive", $.image_phrase)),
+          seq(kw("MOUSE-POINTER"), field("mouse_pointer", $._expression)),
+          seq(kw("LABEL"), field("label", $._identifier_or_string_literal)),
+          seq(kw("LIKE"), field("like", $.identifier)),
+          seq(
+            choice(kw("SIZE"), kw("SIZE-CHARS"), kw("SIZE-PIXELS")),
+            field("width", $._expression),
+            kw("BY"),
+            field("height", $._expression),
+          ),
+          seq(alias(kw("NO-FOCUS"), $.no_focus), optional(alias(kw("FLAT-BUTTON"), $.flat_button))),
+          alias(kw("NO-CONVERT-3D-COLORS"), $.no_convert_3d_colors),
+          seq(
+            kw("TOOLTIP"),
+            field("tooltip", choice($._identifier_or_string_literal, $.null_literal)),
+          ),
+        ),
       ),
-      seq(alias(kw("NO-FOCUS"), $.no_focus), optional(alias(kw("FLAT-BUTTON"), $.flat_button))),
-      alias(kw("NO-CONVERT-3D-COLORS"), $.no_convert_3d_colors),
-      seq(kw("TOOLTIP"), field("tooltip", choice($._identifier_or_string_literal, $.null_literal))),
     ),
 });
