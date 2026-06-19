3bd94e8d36f5df52389ada1fd9d8a037bb170311
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-19 02:15:49 +0200

    grammar: inline editor options
    ACTION_COUNT: 54927 -> 54882 (-45)
    STATE_COUNT: 38457 -> 38448 (-9)
    LARGE_STATE_COUNT: 17087 -> 17078 (-9)

--- grammar/phrases/editor.js
+++ grammar/phrases/editor.js
@@ -17,21 +17,20 @@ module.exports = ({ kw }) => ({
           field("inner_chars", $.number_literal),
         ),
       ),
-      optional($.__editor_options),
-    ),
-
-  __editor_options: ($) =>
-    repeat1(
-      choice(
-        seq(kw("BUFFER-CHARS"), field("buffer_chars", $.number_literal)),
-        seq(kw("BUFFER-LINES"), field("buffer_lines", $.number_literal)),
-        alias(kw("LARGE"), $.large),
-        seq(kw("MAX-CHARS"), field("max_chars", $.number_literal)),
-        alias(kw("NO-BOX"), $.no_box),
-        alias(kw("NO-WORD-WRAP"), $.no_word_wrap),
-        alias(kw("SCROLLBAR-HORIZONTAL"), $.scrollbar_horizontal),
-        alias(kw("SCROLLBAR-VERTICAL"), $.scrollbar_vertical),
-        seq(kw("TOOLTIP"), field("tooltip", $._expression)),
+      optional(
+        repeat1(
+          choice(
+            seq(kw("BUFFER-CHARS"), field("buffer_chars", $.number_literal)),
+            seq(kw("BUFFER-LINES"), field("buffer_lines", $.number_literal)),
+            alias(kw("LARGE"), $.large),
+            seq(kw("MAX-CHARS"), field("max_chars", $.number_literal)),
+            alias(kw("NO-BOX"), $.no_box),
+            alias(kw("NO-WORD-WRAP"), $.no_word_wrap),
+            alias(kw("SCROLLBAR-HORIZONTAL"), $.scrollbar_horizontal),
+            alias(kw("SCROLLBAR-VERTICAL"), $.scrollbar_vertical),
+            seq(kw("TOOLTIP"), field("tooltip", $._expression)),
+          ),
+        ),
       ),
     ),
 });
