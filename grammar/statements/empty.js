2632f08271fb44ea1164d495a5e5e4d1c24e6901
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:30:03 +0200

    grammar: extract empty temp table prefix
    ACTION_COUNT: 52192 -> 52183 (-9)
    STATE_COUNT: 36234 -> 36231 (-3)
    PARSER_C_SIZE_BYTES: +4996

--- grammar/statements/empty.js
+++ grammar/statements/empty.js
@@ -1,6 +1,7 @@
 module.exports = ({ kw }) => ({
-  empty_temp_table_statement: ($) => seq(kw("EMPTY"), $.__empty_temp_table_body, $._terminator),
+  empty_temp_table_statement: ($) => seq($.__empty_temp_table_prefix, $._terminator),
 
+  __empty_temp_table_prefix: ($) => seq(kw("EMPTY"), $.__empty_temp_table_body),
   __empty_temp_table_body: ($) =>
     seq(kw("TEMP-TABLE"), field("name", $.identifier), optional(alias(kw("NO-ERROR"), $.no_error))),
 });
