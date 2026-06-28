871c184153e8f66cc739f785c0db5ec4bbe1f251
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 05:49:28 +0200

    grammar: extract leave statement prefix
    ACTION_COUNT: 52440 -> 52421 (-19)
    STATE_COUNT: 36326 -> 36318 (-8)
    LARGE_STATE_COUNT: 16263 -> 16259 (-4)
    PARSER_C_SIZE_BYTES: -46585

--- grammar/statements/leave.js
+++ grammar/statements/leave.js
@@ -1,3 +1,5 @@
 module.exports = ({ kw }) => ({
-  leave_statement: ($) => seq(kw("LEAVE"), optional(field("label", $.identifier)), $._terminator),
+  leave_statement: ($) => seq($.__leave_prefix, $._terminator),
+
+  __leave_prefix: ($) => seq(kw("LEAVE"), optional(field("label", $.identifier))),
 });
