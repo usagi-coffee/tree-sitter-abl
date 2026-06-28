60a2a259dd9d27f9339ba4507e467975f1af0b0b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 05:20:20 +0200

    grammar: extract underline statement prefix
    ACTION_COUNT: 52599 -> 52592 (-7)
    STATE_COUNT: 36439 -> 36438 (-1)
    LARGE_STATE_COUNT: 16349 -> 16347 (-2)
    PARSER_C_SIZE_BYTES: -614

--- grammar/statements/underline.js
+++ grammar/statements/underline.js
@@ -1,12 +1,9 @@
 module.exports = ({ kw }) => ({
-  underline_statement: ($) => seq(kw("UNDERLINE"), $.__underline_body, $._terminator),
+  underline_statement: ($) => seq($.__underline_prefix, $.__underline_body, $._terminator),
 
+  __underline_prefix: ($) => seq(kw("UNDERLINE"), optional($._stream_phrase)),
   __underline_body: ($) =>
-    seq(
-      optional($._stream_phrase),
-      repeat1(field("field", $.__underline_field)),
-      optional($.frame_phrase),
-    ),
+    seq(repeat1(field("field", $.__underline_field)), optional($.frame_phrase)),
 
   __underline_field: ($) => $._expression,
 });
