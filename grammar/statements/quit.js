129c161321d58c01773ef521eee73f29464224df
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 19:54:26 +0200

    grammar: extract quit prefix
    ACTION_COUNT: 51314 -> 51305 (-9)
    STATE_COUNT: 35587 -> 35588 (+1)
    PARSER_C_SIZE_BYTES: +4584

--- grammar/statements/quit.js
+++ grammar/statements/quit.js
@@ -1,3 +1,5 @@
 module.exports = ({ kw }) => ({
-  quit_statement: ($) => seq(kw("QUIT"), $._terminator),
+  quit_statement: ($) => seq($.__quit_prefix, $._terminator),
+
+  __quit_prefix: ($) => kw("QUIT"),
 });
