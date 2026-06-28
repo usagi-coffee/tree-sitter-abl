7ea315572c0065c37568335c2e194afada7d6071
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 05:26:32 +0200

    grammar: extract down statement prefix
    ACTION_COUNT: 52570 -> 52559 (-11)
    STATE_COUNT: 36422 -> 36414 (-8)
    LARGE_STATE_COUNT: 16335 -> 16327 (-8)
    PARSER_C_SIZE_BYTES: -65430

--- grammar/statements/down.js
+++ grammar/statements/down.js
@@ -1,3 +1,5 @@
 module.exports = ({ kw }) => ({
-  down_statement: ($) => seq(kw("DOWN"), optional($._up_down_tail), $._terminator),
+  down_statement: ($) => seq($.__down_prefix, $._terminator),
+
+  __down_prefix: ($) => seq(kw("DOWN"), optional($._up_down_tail)),
 });
