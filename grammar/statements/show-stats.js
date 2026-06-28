65b32eaa3be95d9f69a94fb42f1576100ba821c7
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 05:53:46 +0200

    grammar: extract show stats prefix
    ACTION_COUNT: 52391 -> 52372 (-19)
    STATE_COUNT: 36302 -> 36294 (-8)
    LARGE_STATE_COUNT: 16247 -> 16243 (-4)
    PARSER_C_SIZE_BYTES: -47892

--- grammar/statements/show-stats.js
+++ grammar/statements/show-stats.js
@@ -1,4 +1,5 @@
 module.exports = ({ kw }) => ({
-  show_stats_statement: ($) =>
-    seq(kw("SHOW-STATS"), optional(alias(kw("CLEAR"), $.clear)), $._terminator),
+  show_stats_statement: ($) => seq($.__show_stats_prefix, $._terminator),
+
+  __show_stats_prefix: ($) => seq(kw("SHOW-STATS"), optional(alias(kw("CLEAR"), $.clear))),
 });
