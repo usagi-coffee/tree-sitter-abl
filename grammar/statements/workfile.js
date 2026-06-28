ef5783ed4221d8ec19261a5eee08aa5194fa4e0b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 08:00:16 +0200

    grammar: extract workfile definition prefix
    ACTION_COUNT: 51823 -> 51807 (-16)
    STATE_COUNT: 35971 -> 35951 (-20)
    LARGE_STATE_COUNT: 16147 -> 16143 (-4)
    PARSER_C_SIZE_BYTES: -52111

--- grammar/statements/workfile.js
+++ grammar/statements/workfile.js
@@ -1,11 +1,12 @@
 module.exports = ({ kw }) => ({
   // Workfile is equivalent to WORK-TABLE
-  workfile_definition: ($) =>
+  workfile_definition: ($) => seq($.__workfile_prefix, $._terminator),
+
+  __workfile_prefix: ($) =>
     seq(
       kw("DEFINE", { offset: 3 }),
       optional($._work_table_modifier),
       kw("WORKFILE"),
       $._work_table_body,
-      $._terminator,
     ),
 });
