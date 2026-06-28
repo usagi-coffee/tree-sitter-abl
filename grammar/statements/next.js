0b9271cc55c38aa23532b444d1002a149111827b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 05:51:00 +0200

    grammar: extract next statement prefix
    ACTION_COUNT: 52421 -> 52402 (-19)
    STATE_COUNT: 36318 -> 36310 (-8)
    LARGE_STATE_COUNT: 16259 -> 16255 (-4)
    PARSER_C_SIZE_BYTES: -50001

--- grammar/statements/next.js
+++ grammar/statements/next.js
@@ -1,4 +1,5 @@
 module.exports = ({ kw }) => ({
-  next_statement: ($) =>
-    prec.right(seq(kw("NEXT"), optional(field("label", $.identifier)), $._terminator)),
+  next_statement: ($) => prec.right(seq($.__next_prefix, $._terminator)),
+
+  __next_prefix: ($) => seq(kw("NEXT"), optional(field("label", $.identifier))),
 });
