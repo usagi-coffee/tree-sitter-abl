989718659a5f5411e0b00cc37f4216e7d0d82462
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:04:13 +0200

    grammar: extract terminal prefix
    ACTION_COUNT: 52336 -> 52327 (-9)
    STATE_COUNT: 36282 -> 36279 (-3)
    PARSER_C_SIZE_BYTES: +4774

--- grammar/statements/terminal.js
+++ grammar/statements/terminal.js
@@ -1,5 +1,6 @@
 module.exports = ({ kw }) => ({
-  terminal_statement: ($) => seq(kw("TERMINAL"), $.__terminal_body, $._terminator),
+  terminal_statement: ($) => seq($.__terminal_prefix, $._terminator),
 
+  __terminal_prefix: ($) => seq(kw("TERMINAL"), $.__terminal_body),
   __terminal_body: ($) => seq("=", field("terminal", $._expression)),
 });
