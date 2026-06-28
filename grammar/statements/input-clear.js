f70f8584a6d78a75a1af73f4003cab6f9c7fa7d6
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 04:09:02 +0200

    grammar: inline input clear body
    ACTION_COUNT: 53292 -> 53298 (+6)
    STATE_COUNT: 36687 -> 36686 (-1)
    LARGE_STATE_COUNT: 16371 -> 16371 (0)
    PARSER_C_SIZE_BYTES: +8

--- grammar/statements/input-clear.js
+++ grammar/statements/input-clear.js
@@ -1,5 +1,3 @@
 module.exports = ({ kw }) => ({
-  input_clear_statement: ($) => seq(kw("INPUT"), $.__input_clear_body, $._terminator),
-
-  __input_clear_body: ($) => seq(kw("CLEAR")),
+  input_clear_statement: ($) => seq(kw("INPUT"), kw("CLEAR"), $._terminator),
 });
