2dad6e83fbf9e6912ab8e4e26cafe0429f4638fa
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:05:46 +0200

    grammar: extract set size prefix
    ACTION_COUNT: 52327 -> 52318 (-9)
    STATE_COUNT: 36279 -> 36276 (-3)
    PARSER_C_SIZE_BYTES: -1647

--- grammar/statements/set-size.js
+++ grammar/statements/set-size.js
@@ -1,6 +1,7 @@
 module.exports = ({ kw }) => ({
-  set_size_statement: ($) => seq(kw("SET-SIZE"), $.__set_size_body, $._terminator),
+  set_size_statement: ($) => seq($.__set_size_prefix, $._terminator),
 
+  __set_size_prefix: ($) => seq(kw("SET-SIZE"), $.__set_size_body),
   __set_size_body: ($) =>
     seq(
       "(",
