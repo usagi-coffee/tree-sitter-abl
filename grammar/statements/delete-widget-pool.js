b20335fd25e80862032e724f00fef0141b836d3e
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 19:06:45 +0200

    grammar: extract delete widget pool prefix
    ACTION_COUNT: 51471 -> 51455 (-16)
    STATE_COUNT: 35633 -> 35625 (-8)
    LARGE_STATE_COUNT: 16059 -> 16055 (-4)
    PARSER_C_SIZE_BYTES: -46626

--- grammar/statements/delete-widget-pool.js
+++ grammar/statements/delete-widget-pool.js
@@ -1,4 +1,6 @@
 module.exports = ({ kw }) => ({
-  delete_widget_pool_statement: ($) =>
-    seq(kw("DELETE"), kw("WIDGET-POOL"), optional(field("pool", $.identifier)), $._terminator),
+  delete_widget_pool_statement: ($) => seq($.__delete_widget_pool_prefix, $._terminator),
+
+  __delete_widget_pool_prefix: ($) =>
+    seq(kw("DELETE"), kw("WIDGET-POOL"), optional(field("pool", $.identifier))),
 });
