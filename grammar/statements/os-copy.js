279553febe5248b18ff0e12cbe784361371c72d1
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:11:57 +0200

    grammar: extract os copy prefix
    ACTION_COUNT: 52291 -> 52282 (-9)
    STATE_COUNT: 36267 -> 36264 (-3)
    PARSER_C_SIZE_BYTES: -7751

--- grammar/statements/os-copy.js
+++ grammar/statements/os-copy.js
@@ -1,5 +1,6 @@
 module.exports = ({ kw }) => ({
-  os_copy_statement: ($) => seq(kw("OS-COPY"), $.__os_copy_body, $._terminator),
+  os_copy_statement: ($) => seq($.__os_copy_prefix, $._terminator),
 
+  __os_copy_prefix: ($) => seq(kw("OS-COPY"), $.__os_copy_body),
   __os_copy_body: ($) => seq(field("source", $._os_filename), field("target", $._os_filename)),
 });
