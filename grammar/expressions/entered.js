d0827a2435c5c4373e720924863f85a4efd194d5
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 22:30:15 +0200

    grammar: share entered expression keyword
    ACTION_COUNT: 49226 -> 48686 (-540)
    STATE_COUNT: 35597 -> 35781 (+184)
    LARGE_STATE_COUNT: 16043 -> 16192 (+149)
    PARSER_C_SIZE_BYTES: +1101665

--- grammar/expressions/entered.js
+++ grammar/expressions/entered.js
@@ -3,11 +3,13 @@ module.exports = ({ kw }) => ({
     choice(
       prec.right(
         -2,
-        seq(field("field", $.identifier), optional(alias(kw("NOT"), $.not)), kw("ENTERED")),
+        seq(field("field", $.identifier), optional(alias(kw("NOT"), $.not)), $.__entered_keyword),
       ),
       prec.right(
         -2,
-        seq(optional(alias(kw("NOT"), $.not)), kw("ENTERED"), field("field", $.identifier)),
+        seq(optional(alias(kw("NOT"), $.not)), $.__entered_keyword, field("field", $.identifier)),
       ),
     ),
+
+  __entered_keyword: ($) => kw("ENTERED"),
 });
