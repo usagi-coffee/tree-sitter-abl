aff619f1e789124447f6a4949013f76c1692db84
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 04:05:42 +0200

    grammar: inline accum expression body
    ACTION_COUNT: 53290 -> 53286 (-4)
    STATE_COUNT: 36815 -> 36688 (-127)
    LARGE_STATE_COUNT: 16462 -> 16371 (-91)
    PARSER_C_SIZE_BYTES: -615166

--- grammar/expressions/accum.js
+++ grammar/expressions/accum.js
@@ -1,8 +1,10 @@
 module.exports = ({ kw }) => ({
-  accum_expression: ($) => seq(kw("ACCUM"), $.__accum_expression_body),
-
-  __accum_expression_body: ($) =>
-    seq(field("operation", repeat1($.__accum_aggregate)), field("field", $.__accum_expression)),
+  accum_expression: ($) =>
+    seq(
+      kw("ACCUM"),
+      field("operation", repeat1($.__accum_aggregate)),
+      field("field", $.__accum_expression),
+    ),
 
   __accum_aggregate: ($) => $.aggregate_phrase,
   __accum_expression: ($) => $._expression,
