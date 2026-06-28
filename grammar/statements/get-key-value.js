446f993f5ee4b2d7ae1bb9169d7cce3239aa368b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:26:46 +0200

    grammar: extract get key value prefix
    ACTION_COUNT: 52210 -> 52201 (-9)
    STATE_COUNT: 36240 -> 36237 (-3)
    PARSER_C_SIZE_BYTES: +2103

--- grammar/statements/get-key-value.js
+++ grammar/statements/get-key-value.js
@@ -1,6 +1,7 @@
 module.exports = ({ kw }) => ({
-  get_key_value_statement: ($) => seq(kw("GET-KEY-VALUE"), $.__get_key_value_body, $._terminator),
+  get_key_value_statement: ($) => seq($.__get_key_value_prefix, $._terminator),
 
+  __get_key_value_prefix: ($) => seq(kw("GET-KEY-VALUE"), $.__get_key_value_body),
   __get_key_value_body: ($) =>
     seq(
       kw("SECTION"),
