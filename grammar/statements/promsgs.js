8423b164fcbd53db595cb90687dddcf9e4140c02
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:07:20 +0200

    grammar: extract promsgs prefix
    ACTION_COUNT: 52318 -> 52309 (-9)
    STATE_COUNT: 36276 -> 36273 (-3)
    PARSER_C_SIZE_BYTES: +6910

--- grammar/statements/promsgs.js
+++ grammar/statements/promsgs.js
@@ -1,5 +1,6 @@
 module.exports = ({ kw }) => ({
-  promsgs_statement: ($) => seq(kw("PROMSGS"), $.__promsgs_body, $._terminator),
+  promsgs_statement: ($) => seq($.__promsgs_prefix, $._terminator),
 
+  __promsgs_prefix: ($) => seq(kw("PROMSGS"), $.__promsgs_body),
   __promsgs_body: ($) => seq("=", field("value", $._expression)),
 });
