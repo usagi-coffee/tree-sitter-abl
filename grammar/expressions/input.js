bc863b10c3d467722f3bb5f8c7d100206288cf76
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 04:55:30 +0200

    grammar: extract input frame phrase
    ACTION_COUNT: 53216 -> 52684 (-532)
    STATE_COUNT: 36622 -> 36489 (-133)
    LARGE_STATE_COUNT: 16362 -> 16362 (0)
    PARSER_C_SIZE_BYTES: -85848

--- grammar/expressions/input.js
+++ grammar/expressions/input.js
@@ -2,7 +2,8 @@ module.exports = ({ kw }) => ({
   input_expression: ($) =>
     seq(
       kw("INPUT"),
-      optional(seq(kw("FRAME"), field("frame", $.identifier))),
+      optional($.__input_frame_phrase),
       field("field", choice($._identifier_or_qualified_name, $.object_access, $.array_access)),
     ),
+  __input_frame_phrase: ($) => seq(kw("FRAME"), field("frame", $.identifier)),
 });
