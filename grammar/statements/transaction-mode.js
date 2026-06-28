0484e21d35229028ccbfd05bd256e884cfe6bd05
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 05:58:11 +0200

    grammar: extract transaction mode prefix
    ACTION_COUNT: 52372 -> 52363 (-9)
    STATE_COUNT: 36294 -> 36291 (-3)
    PARSER_C_SIZE_BYTES: +4265

--- grammar/statements/transaction-mode.js
+++ grammar/statements/transaction-mode.js
@@ -1,6 +1,6 @@
 module.exports = ({ kw }) => ({
-  transaction_mode_statement: ($) =>
-    seq(kw("TRANSACTION-MODE"), $.__transaction_mode_body, $._terminator),
+  transaction_mode_statement: ($) => seq($.__transaction_mode_prefix, $._terminator),
 
+  __transaction_mode_prefix: ($) => seq(kw("TRANSACTION-MODE"), $.__transaction_mode_body),
   __transaction_mode_body: ($) => seq(kw("AUTOMATIC"), optional(alias(kw("CHAINED"), $.chained))),
 });
