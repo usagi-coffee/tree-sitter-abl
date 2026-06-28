1b2d8b10a3be1cf8a676335ae3e54bb4b467843a
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:19:51 +0200

    grammar: extract validate prefix
    ACTION_COUNT: 52246 -> 52237 (-9)
    STATE_COUNT: 36252 -> 36249 (-3)
    PARSER_C_SIZE_BYTES: -1538

--- grammar/statements/validate.js
+++ grammar/statements/validate.js
@@ -1,5 +1,6 @@
 module.exports = ({ kw }) => ({
-  validate_statement: ($) => seq(kw("VALIDATE"), $.__validate_body, $._no_error_terminator),
+  validate_statement: ($) => seq($.__validate_prefix, $._no_error_terminator),
 
+  __validate_prefix: ($) => seq(kw("VALIDATE"), $.__validate_body),
   __validate_body: ($) => seq(field("record", $._identifier_or_qualified_name)),
 });
