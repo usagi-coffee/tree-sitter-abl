6870a10126430c85f42d649521f489c638da4dfd
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:21:29 +0200

    grammar: extract use prefix
    ACTION_COUNT: 52237 -> 52228 (-9)
    STATE_COUNT: 36249 -> 36246 (-3)
    LARGE_STATE_COUNT: 16239 -> 16235 (-4)
    PARSER_C_SIZE_BYTES: -16245

--- grammar/statements/use.js
+++ grammar/statements/use.js
@@ -1,5 +1,6 @@
 module.exports = ({ kw }) => ({
-  use_statement: ($) => seq(kw("USE"), $.__use_body, $._no_error_terminator),
+  use_statement: ($) => seq($.__use_prefix, $._no_error_terminator),
 
+  __use_prefix: ($) => seq(kw("USE"), $.__use_body),
   __use_body: ($) => seq(field("environment", $._expression)),
 });
