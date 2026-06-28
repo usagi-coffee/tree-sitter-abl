ec2fe5a03808ac15b2b99c4ffd98762ca72f8ff7
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 07:05:19 +0200

    grammar: extract next prompt prefix
    ACTION_COUNT: 52057 -> 52048 (-9)
    STATE_COUNT: 36189 -> 36186 (-3)
    PARSER_C_SIZE_BYTES: -209

--- grammar/statements/next-prompt.js
+++ grammar/statements/next-prompt.js
@@ -1,6 +1,7 @@
 module.exports = ({ kw }) => ({
-  next_prompt_statement: ($) => seq(kw("NEXT-PROMPT"), $.__next_prompt_body, $._terminator),
+  next_prompt_statement: ($) => seq($.__next_prompt_prefix, $._terminator),
 
+  __next_prompt_prefix: ($) => seq(kw("NEXT-PROMPT"), $.__next_prompt_body),
   __next_prompt_body: ($) =>
     seq(field("field", $._identifier_or_qualified_name), optional($.frame_phrase)),
 });
