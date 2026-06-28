0a5141cec4800bda6f25e8d0abe68754e5aba91b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 07:13:04 +0200

    grammar: extract unload prefix
    ACTION_COUNT: 52030 -> 52021 (-9)
    STATE_COUNT: 36180 -> 36177 (-3)
    LARGE_STATE_COUNT: 16219 -> 16215 (-4)
    PARSER_C_SIZE_BYTES: -13648

--- grammar/statements/unload.js
+++ grammar/statements/unload.js
@@ -1,4 +1,5 @@
 module.exports = ({ kw }) => ({
-  unload_statement: ($) => seq(kw("UNLOAD"), $.__unload_body, $._no_error_terminator),
+  unload_statement: ($) => seq($.__unload_prefix, $._no_error_terminator),
+  __unload_prefix: ($) => seq(kw("UNLOAD"), $.__unload_body),
   __unload_body: ($) => seq(field("file", $._expression)),
 });
