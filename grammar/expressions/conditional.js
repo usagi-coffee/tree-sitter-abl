4c714b900e7e907d7d8a1273cbcb40b77875e404
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 20:45:30 +0200

    grammar: extract conditional expression prefix
    ACTION_COUNT: 50044 -> 49760 (-284)
    STATE_COUNT: 35594 -> 35595 (+1)
    PARSER_C_SIZE_BYTES: +157450

--- grammar/expressions/conditional.js
+++ grammar/expressions/conditional.js
@@ -1,4 +1,13 @@
 module.exports = ({ kw }) => ({
   conditional_expression: ($) =>
-    seq(kw("IF"), $._expression, kw("THEN"), $._expression, kw("ELSE"), $._expression),
+    seq(
+      $.__conditional_prefix,
+      $._expression,
+      kw("THEN"),
+      $._expression,
+      kw("ELSE"),
+      $._expression,
+    ),
+
+  __conditional_prefix: ($) => kw("IF"),
 });
