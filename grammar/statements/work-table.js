7fc10dae3bcaebd7e2c1e4d2a67574f4cd31f579
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 07:58:13 +0200

    grammar: extract work table definition prefix
    ACTION_COUNT: 51839 -> 51823 (-16)
    STATE_COUNT: 35987 -> 35971 (-16)
    LARGE_STATE_COUNT: 16151 -> 16147 (-4)
    PARSER_C_SIZE_BYTES: -48771

--- grammar/statements/work-table.js
+++ grammar/statements/work-table.js
@@ -1,11 +1,12 @@
 module.exports = ({ kw }) => ({
-  work_table_definition: ($) =>
+  work_table_definition: ($) => seq($.__work_table_prefix, $._terminator),
+
+  __work_table_prefix: ($) =>
     seq(
       kw("DEFINE", { offset: 3 }),
       optional($._work_table_modifier),
       kw("WORK-TABLE"),
       $._work_table_body,
-      $._terminator,
     ),
 
   _work_table_body: ($) =>
