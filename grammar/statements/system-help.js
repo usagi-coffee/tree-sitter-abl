039748b1a88da868527f51a0125357b8800391eb
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 19:30:58 +0200

    grammar: extract system help prefix
    ACTION_COUNT: 51375 -> 51366 (-9)
    STATE_COUNT: 35594 -> 35591 (-3)
    LARGE_STATE_COUNT: 16047 -> 16043 (-4)
    PARSER_C_SIZE_BYTES: -18145

--- grammar/statements/system-help.js
+++ grammar/statements/system-help.js
@@ -1,4 +1,5 @@
 module.exports = ({ kw }) => ({
-  system_help_statement: ($) =>
-    seq(kw("SYSTEM-HELP"), field("topic", $._expression), $._no_error_terminator),
+  system_help_statement: ($) => seq($.__system_help_prefix, $._no_error_terminator),
+
+  __system_help_prefix: ($) => seq(kw("SYSTEM-HELP"), field("topic", $._expression)),
 });
