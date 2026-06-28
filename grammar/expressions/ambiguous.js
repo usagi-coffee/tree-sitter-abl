ff96c783365e8d20691fa3a7518a8793645b01bc
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 20:34:17 +0200

    grammar: extract ambiguous expression prefix
    ACTION_COUNT: 51044 -> 50794 (-250)
    STATE_COUNT: 35590 -> 35591 (+1)
    PARSER_C_SIZE_BYTES: +120443

--- grammar/expressions/ambiguous.js
+++ grammar/expressions/ambiguous.js
@@ -1,4 +1,6 @@
 module.exports = ({ kw }) => ({
   ambiguous_expression: ($) =>
-    seq(kw("AMBIGUOUS"), field("record", $._record_or_parenthesized_record)),
+    seq($.__ambiguous_prefix, field("record", $._record_or_parenthesized_record)),
+
+  __ambiguous_prefix: ($) => kw("AMBIGUOUS"),
 });
