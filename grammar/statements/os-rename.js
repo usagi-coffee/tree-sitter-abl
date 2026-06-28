5ab1d02c285da8ee1d332a4a46e901f8c5163aac
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:13:25 +0200

    grammar: extract os rename prefix
    ACTION_COUNT: 52282 -> 52273 (-9)
    STATE_COUNT: 36264 -> 36261 (-3)
    PARSER_C_SIZE_BYTES: -6907

--- grammar/statements/os-rename.js
+++ grammar/statements/os-rename.js
@@ -1,5 +1,6 @@
 module.exports = ({ kw }) => ({
-  os_rename_statement: ($) => seq(kw("OS-RENAME"), $.__os_rename_body, $._terminator),
+  os_rename_statement: ($) => seq($.__os_rename_prefix, $._terminator),
 
+  __os_rename_prefix: ($) => seq(kw("OS-RENAME"), $.__os_rename_body),
   __os_rename_body: ($) => seq(field("source", $._os_filename), field("target", $._os_filename)),
 });
