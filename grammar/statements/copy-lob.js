e1058641011e24bdda4984fda87f46e1e8d5b114
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-06-28 06:48:55 +0200

    grammar: extract copy lob prefix
    ACTION_COUNT: 52111 -> 52102 (-9)
    STATE_COUNT: 36207 -> 36204 (-3)
    LARGE_STATE_COUNT: 16227 -> 16223 (-4)
    PARSER_C_SIZE_BYTES: -15527

--- grammar/statements/copy-lob.js
+++ grammar/statements/copy-lob.js
@@ -1,6 +1,7 @@
 module.exports = ({ kw }) => ({
-  copy_lob_statement: ($) => seq(kw("COPY-LOB"), $.__copy_lob_body, $._no_error_terminator),
+  copy_lob_statement: ($) => seq($.__copy_lob_prefix, $._no_error_terminator),
 
+  __copy_lob_prefix: ($) => seq(kw("COPY-LOB"), $.__copy_lob_body),
   __copy_lob_body: ($) =>
     seq(
       optional(kw("FROM")),
