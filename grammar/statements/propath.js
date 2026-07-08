e91b06d9949816d1cf199c59f6318bb78f3f64bf
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-08 23:55:28 +0200

    grammar: inline propath body
    ACTION_COUNT: 48266 -> 48264 (-2)
    STATE_COUNT: 35494 -> 35493 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -244

--- grammar/statements/propath.js
+++ grammar/statements/propath.js
@@ -1,6 +1,5 @@
 module.exports = ({ kw }) => ({
   propath_statement: ($) => seq($.__propath_prefix, $._terminator),
 
-  __propath_prefix: ($) => seq(kw("PROPATH"), $.__propath_body),
-  __propath_body: ($) => seq(choice("=", "+="), field("value", $._expression)),
+  __propath_prefix: ($) => seq(kw("PROPATH"), choice("=", "+="), field("value", $._expression)),
 });
