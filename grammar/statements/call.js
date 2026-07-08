b254fd8221b22f6aa0253b322c8844f782db30e6
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:58:05 +0200

    grammar: inline call body
    ACTION_COUNT: 48228 -> 48226 (-2)
    STATE_COUNT: 35472 -> 35471 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: -3863

--- grammar/statements/call.js
+++ grammar/statements/call.js
@@ -1,9 +1,8 @@
 module.exports = ({ kw }) => ({
   call_statement: ($) => seq($.__call_prefix, $._terminator),
 
-  __call_prefix: ($) => seq(kw("CALL"), $.__call_body),
-  __call_body: ($) =>
-    seq(field("routine", $.identifier), repeat(alias($.__call_argument, $.argument))),
+  __call_prefix: ($) =>
+    seq(kw("CALL"), field("routine", $.identifier), repeat(alias($.__call_argument, $.argument))),
 
   __call_argument: ($) => $._expression,
 });
