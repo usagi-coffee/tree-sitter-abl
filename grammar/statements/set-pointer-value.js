8432133f4a054a14dfe7e0cd8bc21f96805a5dd8
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:20:04 +0200

    grammar: inline set pointer value body
    ACTION_COUNT: 48246 -> 48244 (-2)
    STATE_COUNT: 35484 -> 35483 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -953

--- grammar/statements/set-pointer-value.js
+++ grammar/statements/set-pointer-value.js
@@ -1,10 +1,9 @@
 module.exports = ({ kw }) => ({
   set_pointer_value_statement: ($) => seq($.__set_pointer_value_prefix, $._terminator),
 
-  __set_pointer_value_prefix: ($) => seq(kw("SET-POINTER-VALUE"), $.__set_pointer_value_body),
-
-  __set_pointer_value_body: ($) =>
+  __set_pointer_value_prefix: ($) =>
     seq(
+      kw("SET-POINTER-VALUE"),
       "(",
       field("target", choice($._identifier_or_qualified_name, $.object_access)),
       ")",
