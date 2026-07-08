6f0da9e13707e83ab5e65e193f3745b6f9df8819
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:13:03 +0200

    grammar: inline load body
    ACTION_COUNT: 48252 -> 48250 (-2)
    STATE_COUNT: 35487 -> 35486 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -2530

--- grammar/statements/load.js
+++ grammar/statements/load.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   load_statement: ($) => seq($.__load_prefix, $._no_error_terminator),
 
-  __load_prefix: ($) => seq(kw("LOAD"), $.__load_body),
-  __load_body: ($) =>
+  __load_prefix: ($) =>
     seq(
+      kw("LOAD"),
       field("file", $._expression),
       optional(seq(kw("DIR"), field("dir", $._expression))),
       optional(seq(kw("APPLICATION"), field("app", $._expression))),
