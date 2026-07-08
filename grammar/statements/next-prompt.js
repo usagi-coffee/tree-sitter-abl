c40205a2d24909242172a08aea0ac802c13aeb23
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 01:01:35 +0200

    grammar: inline next prompt body
    ACTION_COUNT: 48226 -> 48224 (-2)
    STATE_COUNT: 35471 -> 35470 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: +1518

--- grammar/statements/next-prompt.js
+++ grammar/statements/next-prompt.js
@@ -1,7 +1,10 @@
 module.exports = ({ kw }) => ({
   next_prompt_statement: ($) => seq($.__next_prompt_prefix, $._terminator),
 
-  __next_prompt_prefix: ($) => seq(kw("NEXT-PROMPT"), $.__next_prompt_body),
-  __next_prompt_body: ($) =>
-    seq(field("field", $._identifier_or_qualified_name), optional($.frame_phrase)),
+  __next_prompt_prefix: ($) =>
+    seq(
+      kw("NEXT-PROMPT"),
+      field("field", $._identifier_or_qualified_name),
+      optional($.frame_phrase),
+    ),
 });
