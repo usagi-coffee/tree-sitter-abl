0b7a0aa275e9e45dfec59fcde5729a78dd545034
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 01:03:52 +0200

    grammar: inline overlay body
    ACTION_COUNT: 48224 -> 48222 (-2)
    STATE_COUNT: 35470 -> 35469 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: -865

--- grammar/statements/overlay.js
+++ grammar/statements/overlay.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   overlay_statement: ($) => seq($.__overlay_prefix, $._terminator),
 
-  __overlay_prefix: ($) => seq(kw("OVERLAY"), $.__overlay_body),
-  __overlay_body: ($) =>
+  __overlay_prefix: ($) =>
     seq(
+      kw("OVERLAY"),
       "(",
       field("target", $._expression),
       ",",
