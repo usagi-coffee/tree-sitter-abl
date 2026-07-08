23f8d8757dfd4621294ccda91519f43323c0cacc
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-08 23:44:06 +0200

    grammar: inline delete alias body
    ACTION_COUNT: 48278 -> 48276 (-2)
    STATE_COUNT: 35500 -> 35499 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -794

--- grammar/statements/delete-alias.js
+++ grammar/statements/delete-alias.js
@@ -1,7 +1,5 @@
 module.exports = ({ kw }) => ({
   delete_alias_statement: ($) => seq($.__delete_alias_prefix, $._no_error_terminator),
 
-  __delete_alias_prefix: ($) => seq(kw("DELETE"), kw("ALIAS"), $.__delete_alias_body),
-
-  __delete_alias_body: ($) => seq(field("alias", $._alias_name)),
+  __delete_alias_prefix: ($) => seq(kw("DELETE"), kw("ALIAS"), field("alias", $._alias_name)),
 });
