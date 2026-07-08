fdb9d9abfc4f30821abdf3e443b581c80a36c2c2
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:22:07 +0200

    grammar: inline transaction mode body
    ACTION_COUNT: 48244 -> 48242 (-2)
    STATE_COUNT: 35483 -> 35482 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -709

--- grammar/statements/transaction-mode.js
+++ grammar/statements/transaction-mode.js
@@ -1,6 +1,6 @@
 module.exports = ({ kw }) => ({
   transaction_mode_statement: ($) => seq($.__transaction_mode_prefix, $._terminator),
 
-  __transaction_mode_prefix: ($) => seq(kw("TRANSACTION-MODE"), $.__transaction_mode_body),
-  __transaction_mode_body: ($) => seq(kw("AUTOMATIC"), optional(alias(kw("CHAINED"), $.chained))),
+  __transaction_mode_prefix: ($) =>
+    seq(kw("TRANSACTION-MODE"), kw("AUTOMATIC"), optional(alias(kw("CHAINED"), $.chained))),
 });
