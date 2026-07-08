72f577872c3e6648a288d7c86b30b4a4fddcce9e
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-08 23:53:37 +0200

    grammar: inline promsgs body
    ACTION_COUNT: 48268 -> 48266 (-2)
    STATE_COUNT: 35495 -> 35494 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: +1705

--- grammar/statements/promsgs.js
+++ grammar/statements/promsgs.js
@@ -1,6 +1,5 @@
 module.exports = ({ kw }) => ({
   promsgs_statement: ($) => seq($.__promsgs_prefix, $._terminator),
 
-  __promsgs_prefix: ($) => seq(kw("PROMSGS"), $.__promsgs_body),
-  __promsgs_body: ($) => seq("=", field("value", $._expression)),
+  __promsgs_prefix: ($) => seq(kw("PROMSGS"), "=", field("value", $._expression)),
 });
