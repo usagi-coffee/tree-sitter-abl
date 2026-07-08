de45dbea08dae773614793012e96ef134076a5fe
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:05:35 +0200

    grammar: inline fix codepage body
    ACTION_COUNT: 48258 -> 48256 (-2)
    STATE_COUNT: 35490 -> 35489 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -3775

--- grammar/statements/fix-codepage.js
+++ grammar/statements/fix-codepage.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   fix_codepage_statement: ($) => seq($.__fix_codepage_prefix, $._terminator),
 
-  __fix_codepage_prefix: ($) => seq(kw("FIX-CODEPAGE"), $.__fix_codepage_body),
-  __fix_codepage_body: ($) =>
+  __fix_codepage_prefix: ($) =>
     seq(
+      kw("FIX-CODEPAGE"),
       "(",
       field("target", $._identifier_or_qualified_name),
       ")",
