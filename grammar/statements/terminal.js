d9683997ceffdb8dde5245fb49bf3f9cb41549cb
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-08 23:51:37 +0200

    grammar: inline terminal body
    ACTION_COUNT: 48270 -> 48268 (-2)
    STATE_COUNT: 35496 -> 35495 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -810

--- grammar/statements/terminal.js
+++ grammar/statements/terminal.js
@@ -1,6 +1,5 @@
 module.exports = ({ kw }) => ({
   terminal_statement: ($) => seq($.__terminal_prefix, $._terminator),
 
-  __terminal_prefix: ($) => seq(kw("TERMINAL"), $.__terminal_body),
-  __terminal_body: ($) => seq("=", field("terminal", $._expression)),
+  __terminal_prefix: ($) => seq(kw("TERMINAL"), "=", field("terminal", $._expression)),
 });
