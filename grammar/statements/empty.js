4367ea3338f21d4078226080ceee0b99d775c799
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:25:58 +0200

    grammar: inline empty temp table body
    ACTION_COUNT: 48242 -> 48240 (-2)
    STATE_COUNT: 35482 -> 35481 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -773

--- grammar/statements/empty.js
+++ grammar/statements/empty.js
@@ -1,7 +1,11 @@
 module.exports = ({ kw }) => ({
   empty_temp_table_statement: ($) => seq($.__empty_temp_table_prefix, $._terminator),
 
-  __empty_temp_table_prefix: ($) => seq(kw("EMPTY"), $.__empty_temp_table_body),
-  __empty_temp_table_body: ($) =>
-    seq(kw("TEMP-TABLE"), field("name", $.identifier), optional(alias(kw("NO-ERROR"), $.no_error))),
+  __empty_temp_table_prefix: ($) =>
+    seq(
+      kw("EMPTY"),
+      kw("TEMP-TABLE"),
+      field("name", $.identifier),
+      optional(alias(kw("NO-ERROR"), $.no_error)),
+    ),
 });
