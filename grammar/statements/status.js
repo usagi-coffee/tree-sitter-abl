dd6bb50c81fa2296519e20eadd7f3c15a929b35f
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:14:56 +0200

    grammar: inline status body
    ACTION_COUNT: 48250 -> 48248 (-2)
    STATE_COUNT: 35486 -> 35485 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -953

--- grammar/statements/status.js
+++ grammar/statements/status.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   status_statement: ($) => seq($.__status_prefix, $._terminator),
 
-  __status_prefix: ($) => seq(kw("STATUS"), $.__status_body),
-  __status_body: ($) =>
+  __status_prefix: ($) =>
     seq(
+      kw("STATUS"),
       choice(
         seq(kw("DEFAULT"), optional(field("message", $._expression))),
         seq(kw("INPUT"), optional(choice(kw("OFF"), field("message", $._expression)))),
