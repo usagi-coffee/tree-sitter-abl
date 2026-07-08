854ffae2ab1102b10ebcc3f899f530a69ab1ab7e
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:17:56 +0200

    grammar: inline set size body
    ACTION_COUNT: 48248 -> 48246 (-2)
    STATE_COUNT: 35485 -> 35484 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -848

--- grammar/statements/set-size.js
+++ grammar/statements/set-size.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   set_size_statement: ($) => seq($.__set_size_prefix, $._terminator),
 
-  __set_size_prefix: ($) => seq(kw("SET-SIZE"), $.__set_size_body),
-  __set_size_body: ($) =>
+  __set_size_prefix: ($) =>
     seq(
+      kw("SET-SIZE"),
       "(",
       field("buffer", $._identifier_or_qualified_name),
       ")",
