a7c529b06826b48c66776be688ae83d4a21c9d1a
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:48:02 +0200

    grammar: inline disconnect body
    ACTION_COUNT: 48234 -> 48232 (-2)
    STATE_COUNT: 35478 -> 35477 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -242

--- grammar/statements/disconnect.js
+++ grammar/statements/disconnect.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   disconnect_statement: ($) => seq($.__disconnect_prefix, $._no_error_terminator),
 
-  __disconnect_prefix: ($) => seq(kw("DISCONNECT"), $.__disconnect_body),
-  __disconnect_body: ($) =>
+  __disconnect_prefix: ($) =>
     seq(
+      kw("DISCONNECT"),
       choice(
         seq(kw("VALUE"), "(", field("database", $._expression), ")"),
         field("database", $.identifier),
