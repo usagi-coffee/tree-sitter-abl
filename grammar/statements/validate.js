953e1eec500d14f0793b087892b5d19ea18a5137
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-08 23:47:41 +0200

    grammar: inline validate body
    ACTION_COUNT: 48274 -> 48272 (-2)
    STATE_COUNT: 35498 -> 35497 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -246

--- grammar/statements/validate.js
+++ grammar/statements/validate.js
@@ -1,6 +1,5 @@
 module.exports = ({ kw }) => ({
   validate_statement: ($) => seq($.__validate_prefix, $._no_error_terminator),
 
-  __validate_prefix: ($) => seq(kw("VALIDATE"), $.__validate_body),
-  __validate_body: ($) => seq(field("record", $._identifier_or_qualified_name)),
+  __validate_prefix: ($) => seq(kw("VALIDATE"), field("record", $._identifier_or_qualified_name)),
 });
