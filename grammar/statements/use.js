5ec9c6a8c98151541c9fae0adfc7afb19e05a93d
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-08 23:45:53 +0200

    grammar: inline use body
    ACTION_COUNT: 48276 -> 48274 (-2)
    STATE_COUNT: 35499 -> 35498 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -1624

--- grammar/statements/use.js
+++ grammar/statements/use.js
@@ -1,6 +1,5 @@
 module.exports = ({ kw }) => ({
   use_statement: ($) => seq($.__use_prefix, $._no_error_terminator),
 
-  __use_prefix: ($) => seq(kw("USE"), $.__use_body),
-  __use_body: ($) => seq(field("environment", $._expression)),
+  __use_prefix: ($) => seq(kw("USE"), field("environment", $._expression)),
 });
