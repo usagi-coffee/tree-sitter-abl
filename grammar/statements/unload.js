be3aaa3b0ccc73644900dcdd5ee1b4f9e840f5f0
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-08 23:49:42 +0200

    grammar: inline unload body
    ACTION_COUNT: 48272 -> 48270 (-2)
    STATE_COUNT: 35497 -> 35496 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -1235

--- grammar/statements/unload.js
+++ grammar/statements/unload.js
@@ -1,5 +1,4 @@
 module.exports = ({ kw }) => ({
   unload_statement: ($) => seq($.__unload_prefix, $._no_error_terminator),
-  __unload_prefix: ($) => seq(kw("UNLOAD"), $.__unload_body),
-  __unload_body: ($) => seq(field("file", $._expression)),
+  __unload_prefix: ($) => seq(kw("UNLOAD"), field("file", $._expression)),
 });
