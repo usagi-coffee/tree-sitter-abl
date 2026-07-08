d28405332b504a824097f4b5928829e3605ded94
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 00:07:25 +0200

    grammar: inline get key value body
    ACTION_COUNT: 48256 -> 48254 (-2)
    STATE_COUNT: 35489 -> 35488 (-1)
    LARGE_STATE_COUNT: 15991 -> 15991 (0)
    PARSER_C_SIZE_BYTES: -583

--- grammar/statements/get-key-value.js
+++ grammar/statements/get-key-value.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   get_key_value_statement: ($) => seq($.__get_key_value_prefix, $._terminator),
 
-  __get_key_value_prefix: ($) => seq(kw("GET-KEY-VALUE"), $.__get_key_value_body),
-  __get_key_value_body: ($) =>
+  __get_key_value_prefix: ($) =>
     seq(
+      kw("GET-KEY-VALUE"),
       kw("SECTION"),
       field("section", $._expression),
       choice(
