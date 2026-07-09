0c1f3f4fded4cf4f1ac588d0faeef78a05c4f543
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 22:14:44 +0200

    grammar: inline os-delete body
    ACTION_COUNT: 48208 -> 48206 (-2)
    STATE_COUNT: 35462 -> 35461 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: -1142

--- grammar/statements/os-delete.js
+++ grammar/statements/os-delete.js
@@ -1,9 +1,12 @@
 module.exports = ({ kw }) => ({
   os_delete_statement: ($) => seq($.__os_delete_prefix, $._terminator),
 
-  __os_delete_prefix: ($) => seq(kw("OS-DELETE"), $.__os_delete_body),
-  __os_delete_body: ($) =>
-    seq(repeat1($.__os_delete_target), optional(alias(kw("RECURSIVE"), $.recursive))),
+  __os_delete_prefix: ($) =>
+    seq(
+      kw("OS-DELETE"),
+      repeat1($.__os_delete_target),
+      optional(alias(kw("RECURSIVE"), $.recursive)),
+    ),
 
   __os_delete_target: ($) =>
     choice($._os_filename, seq(kw("VALUE"), "(", field("value", $._os_filename), ")")),
