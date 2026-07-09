d1c6c52362af0e492b63b1f53c44436c89f4359a
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 20:12:59 +0200

    grammar: inline convert body
    ACTION_COUNT: 48222 -> 48220 (-2)
    STATE_COUNT: 35469 -> 35468 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: -705

--- grammar/statements/convert.js
+++ grammar/statements/convert.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   convert_statement: ($) => seq($.__convert_prefix, $._terminator),
 
-  __convert_prefix: ($) => seq(kw("CONVERT"), $.__convert_body),
-  __convert_body: ($) =>
+  __convert_prefix: ($) =>
     seq(
+      kw("CONVERT"),
       kw("SOURCE"),
       kw("CODEPAGE"),
       field("source", $.string_literal),
