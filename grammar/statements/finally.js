7f85855e390c9ac73e15f143d10f1ff78e9856f5
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 22:13:23 +0200

    grammar: inline finally body
    ACTION_COUNT: 48210 -> 48208 (-2)
    STATE_COUNT: 35463 -> 35462 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: -228

--- grammar/statements/finally.js
+++ grammar/statements/finally.js
@@ -1,6 +1,5 @@
 module.exports = ({ kw }) => ({
   finally_statement: ($) => seq($.__finally_prefix, $._terminator),
 
-  __finally_prefix: ($) => seq(kw("FINALLY"), $.__finally_body),
-  __finally_body: ($) => seq($.body, kw("END"), optional(kw("FINALLY"))),
+  __finally_prefix: ($) => seq(kw("FINALLY"), $.body, kw("END"), optional(kw("FINALLY"))),
 });
