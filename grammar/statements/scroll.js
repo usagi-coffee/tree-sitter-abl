61753f6e6cb0a7f4b661bab39aa5f755a69b05f9
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-10 01:37:52 +0200

    grammar: inline scroll tail
    ACTION_COUNT: 48144 -> 48142 (-2)
    STATE_COUNT: 35430 -> 35429 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: +1307

--- grammar/statements/scroll.js
+++ grammar/statements/scroll.js
@@ -1,14 +1,18 @@
 module.exports = ({ kw }) => ({
   scroll_statement: ($) => seq($.__scroll_prefix, $._terminator),
-  __scroll_prefix: ($) => seq(kw("SCROLL"), optional($.__scroll_tail)),
-  __scroll_tail: ($) =>
-    choice(
-      seq(
-        alias(kw("FROM-CURRENT"), $.from_current),
-        optional(choice(seq($.__scroll_direction, optional($.frame_phrase)), $.frame_phrase)),
+  __scroll_prefix: ($) =>
+    seq(
+      kw("SCROLL"),
+      optional(
+        choice(
+          seq(
+            alias(kw("FROM-CURRENT"), $.from_current),
+            optional(choice(seq($.__scroll_direction, optional($.frame_phrase)), $.frame_phrase)),
+          ),
+          seq($.__scroll_direction, optional($.frame_phrase)),
+          $.frame_phrase,
+        ),
       ),
-      seq($.__scroll_direction, optional($.frame_phrase)),
-      $.frame_phrase,
     ),
   __scroll_direction: ($) => choice(alias(kw("UP"), $.up), alias(kw("DOWN"), $.down)),
 });
