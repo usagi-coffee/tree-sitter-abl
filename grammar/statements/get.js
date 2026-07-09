2ff0ae182234874185275707dbfeb1a2e39c0c78
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 20:16:33 +0200

    grammar: inline get body
    ACTION_COUNT: 48220 -> 48218 (-2)
    STATE_COUNT: 35468 -> 35467 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: -927

--- grammar/statements/get.js
+++ grammar/statements/get.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   get_statement: ($) => seq($.__get_prefix, $._terminator),
 
-  __get_prefix: ($) => seq(kw("GET"), $.__get_body),
-  __get_body: ($) =>
+  __get_prefix: ($) =>
     seq(
+      kw("GET"),
       field("direction", choice(kw("FIRST"), kw("NEXT"), kw("PREV"), kw("LAST"), kw("CURRENT"))),
       field("query", $.identifier),
       optional(field("lock", choice(kw("SHARE-LOCK"), kw("EXCLUSIVE-LOCK"), kw("NO-LOCK")))),
