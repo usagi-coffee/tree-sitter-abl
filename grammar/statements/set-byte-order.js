9fe559c5f98b2d36bdfbe3b41a06a6224fc988ea
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 22:01:49 +0200

    grammar: inline set-byte-order body
    ACTION_COUNT: 48216 -> 48214 (-2)
    STATE_COUNT: 35466 -> 35465 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: +1793

--- grammar/statements/set-byte-order.js
+++ grammar/statements/set-byte-order.js
@@ -1,10 +1,9 @@
 module.exports = ({ kw }) => ({
   set_byte_order_statement: ($) => seq($.__set_byte_order_prefix, $._terminator),
 
-  __set_byte_order_prefix: ($) => seq(kw("SET-BYTE-ORDER"), $.__set_byte_order_body),
-
-  __set_byte_order_body: ($) =>
+  __set_byte_order_prefix: ($) =>
     seq(
+      kw("SET-BYTE-ORDER"),
       "(",
       field("buffer", $._expression),
       ")",
