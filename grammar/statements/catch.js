f263f96a673c9f2399f7638a76823f9d38d18069
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 22:18:17 +0200

    grammar: inline catch body
    ACTION_COUNT: 48204 -> 48202 (-2)
    STATE_COUNT: 35460 -> 35459 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: +10667

--- grammar/statements/catch.js
+++ grammar/statements/catch.js
@@ -1,9 +1,9 @@
 module.exports = ({ kw }) => ({
   catch_statement: ($) => seq($.__catch_prefix, $._terminator),
 
-  __catch_prefix: ($) => seq(kw("CATCH"), $.__catch_body),
-  __catch_body: ($) =>
+  __catch_prefix: ($) =>
     seq(
+      kw("CATCH"),
       field("name", $.identifier),
       optional(seq(kw("AS"), optional(kw("CLASS")), field("type", $.qualified_name))),
       $.body,
