b1d35f20f2ac13680d71c10cef85fe044c2e86ba
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-09 22:19:47 +0200

    grammar: inline insert body
    ACTION_COUNT: 48202 -> 48200 (-2)
    STATE_COUNT: 35459 -> 35458 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: +1912

--- grammar/statements/insert.js
+++ grammar/statements/insert.js
@@ -1,8 +1,8 @@
 module.exports = ({ kw }) => ({
   insert_statement: ($) => seq($.__insert_prefix, $._no_error_terminator),
-  __insert_prefix: ($) => seq(kw("INSERT"), $.__insert_body),
-  __insert_body: ($) =>
+  __insert_prefix: ($) =>
     seq(
+      kw("INSERT"),
       field("record", $._identifier_or_qualified_name),
       optional($._except_fields),
       optional(seq(kw("USING"), field("using", $._expression))),
