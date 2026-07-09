96b07c1efbb37f004657dd18d443e0a82a35e459
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-10 01:56:40 +0200

    grammar: inline clear frame
    ACTION_COUNT: 48130 -> 48128 (-2)
    STATE_COUNT: 35427 -> 35426 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: -2229

--- grammar/statements/clear.js
+++ grammar/statements/clear.js
@@ -5,7 +5,7 @@ module.exports = ({ kw }) => ({
       kw("CLEAR"),
       optional(
         choice(
-          seq($.__clear_frame, optional($.__clear_all_no_pause_tail)),
+          seq(kw("FRAME"), field("frame", $.identifier), optional($.__clear_all_no_pause_tail)),
           $.__clear_all_no_pause_tail,
         ),
       ),
@@ -15,5 +15,4 @@ module.exports = ({ kw }) => ({
       seq(alias(kw("ALL"), $.all), optional(alias(kw("NO-PAUSE"), $.no_pause))),
       alias(kw("NO-PAUSE"), $.no_pause),
     ),
-  __clear_frame: ($) => seq(kw("FRAME"), field("frame", $.identifier)),
 });
