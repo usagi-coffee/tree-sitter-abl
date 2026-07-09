af8c5bb682e5c0c0d6138dcee7da85f4927c5c16
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-10 01:10:58 +0200

    grammar: inline readkey body
    ACTION_COUNT: 48154 -> 48152 (-2)
    STATE_COUNT: 35435 -> 35434 (-1)
    LARGE_STATE_COUNT: 15987 -> 15987 (0)
    PARSER_C_SIZE_BYTES: +1296

--- grammar/statements/readkey.js
+++ grammar/statements/readkey.js
@@ -1,10 +1,14 @@
 module.exports = ({ kw }) => ({
   readkey_statement: ($) => seq($.__readkey_prefix, $._terminator),
-  __readkey_prefix: ($) => seq(kw("READKEY"), optional($.__readkey_body)),
-  __readkey_body: ($) =>
-    choice(
-      seq($._stream_phrase, optional(alias($.__readkey_pause_phrase, $.pause_phrase))),
-      alias($.__readkey_pause_phrase, $.pause_phrase),
+  __readkey_prefix: ($) =>
+    seq(
+      kw("READKEY"),
+      optional(
+        choice(
+          seq($._stream_phrase, optional(alias($.__readkey_pause_phrase, $.pause_phrase))),
+          alias($.__readkey_pause_phrase, $.pause_phrase),
+        ),
+      ),
     ),
   __readkey_pause_phrase: ($) => seq(kw("PAUSE"), field("pause", $._expression)),
 });
