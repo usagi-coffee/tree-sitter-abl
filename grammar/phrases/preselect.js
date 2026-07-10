993025fdcf1c411040a4f0a6de0facc3bc937b1d
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-10 13:36:51 +0200

    grammar: inline preselect break-by dispatch
    ACTION_COUNT: 48103 -> 48099 (-4)
    STATE_COUNT: 35416 -> 35414 (-2)
    LARGE_STATE_COUNT: 15986 -> 15986 (0)
    PARSER_C_SIZE_BYTES: -3400

--- grammar/phrases/preselect.js
+++ grammar/phrases/preselect.js
@@ -1,15 +1,19 @@
 module.exports = ({ kw }) => ({
   preselect_phrase: ($) =>
-    seq(kw("PRESELECT"), $.preselect_record_list, optional($.__preselect_break_by_phrase)),
-
-  __preselect_break_by_phrase: ($) =>
-    prec.right(
-      seq(
-        optional(kw("BREAK")),
-        choice($.__preselect_by_phrase, $.__preselect_collate_phrase),
-        repeat(choice($.__preselect_by_phrase, $.__preselect_collate_phrase)),
+    seq(
+      kw("PRESELECT"),
+      $.preselect_record_list,
+      optional(
+        prec.right(
+          seq(
+            optional(kw("BREAK")),
+            choice($.__preselect_by_phrase, $.__preselect_collate_phrase),
+            repeat(choice($.__preselect_by_phrase, $.__preselect_collate_phrase)),
+          ),
+        ),
       ),
     ),
+
   __preselect_by_phrase: ($) =>
     seq(kw("BY"), field("by", $._expression), optional($.__preselect_sort_direction)),
   __preselect_collate_phrase: ($) =>
