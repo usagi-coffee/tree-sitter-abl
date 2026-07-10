0f6fa8417ef8b9ce09a6ac009966cb2e4b87724a
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-10 18:52:12 +0200

    grammar: inline image bgcolor continuation
    ACTION_COUNT: 48011 -> 48009 (-2)
    STATE_COUNT: 35370 -> 35369 (-1)
    LARGE_STATE_COUNT: 15962 -> 15962 (0)
    PARSER_C_SIZE_BYTES: -476

--- grammar/statements/image.js
+++ grammar/statements/image.js
@@ -12,7 +12,21 @@ module.exports = ({ kw }) => ({
           seq(
             kw("BGCOLOR"),
             field("bgcolor", $._expression),
-            optional($.__image_body_after_bgcolor),
+            optional(
+              choice(
+                seq(
+                  kw("FGCOLOR"),
+                  field("fgcolor", $._expression),
+                  optional($.__image_body_after_fgcolor),
+                ),
+                seq(
+                  alias(kw("CONVERT-3D-COLORS"), $.convert_3d_colors),
+                  optional($.__image_body_after_convert_3d_colors),
+                ),
+                seq($.__image_tooltip_phrase, optional($.__image_stretch_transparent_tail)),
+                $.__image_stretch_transparent_tail,
+              ),
+            ),
           ),
           seq(
             kw("FGCOLOR"),
@@ -27,16 +41,6 @@ module.exports = ({ kw }) => ({
           $.__image_stretch_transparent_tail,
         ),
       ),
-    ),
-  __image_body_after_bgcolor: ($) =>
-    choice(
-      seq(kw("FGCOLOR"), field("fgcolor", $._expression), optional($.__image_body_after_fgcolor)),
-      seq(
-        alias(kw("CONVERT-3D-COLORS"), $.convert_3d_colors),
-        optional($.__image_body_after_convert_3d_colors),
-      ),
-      seq($.__image_tooltip_phrase, optional($.__image_stretch_transparent_tail)),
-      $.__image_stretch_transparent_tail,
     ),
   __image_body_after_fgcolor: ($) =>
     choice(
