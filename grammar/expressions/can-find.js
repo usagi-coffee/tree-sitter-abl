9f85d3367787bf91f30660d0d5d53781b22a14ba
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-13 00:10:09 +0200

    grammar: collapse can-find option tail
    ACTION_COUNT: 42946 -> 42814 (-132)
    STATE_COUNT: 33307 -> 33185 (-122)
    LARGE_STATE_COUNT: 14824 -> 14824 (0)
    PARSER_C_SIZE_BYTES: 121297919 -> 121223441 (-74478)
    PARSER_SPEED_BYTES_MS: 61.08

--- grammar/expressions/can-find.js
+++ grammar/expressions/can-find.js
@@ -8,23 +8,68 @@ module.exports = ({ kw }) => ({
       seq(
         optional(choice(kw("FIRST"), kw("LAST"))),
         field("table", $._identifier_or_qualified_name),
-        optional(alias($.__record_query_of_phrase, $.of_phrase)),
-        optional(alias($.__record_query_using_phrase, $.using_phrase)),
-        optional(alias($.__record_query_frame_phrase, $.frame_phrase)),
-        optional(
-          choice(
-            seq(
-              alias($.__record_query_where_phrase, $.where_phrase),
-              optional(alias($.__record_query_lock_phrase, $.no_lock)),
-            ),
-            seq(
-              alias($.__record_query_lock_phrase, $.no_lock),
-              optional(alias($.__record_query_where_phrase, $.where_phrase)),
-            ),
-          ),
-        ),
+        optional($.__record_query_options),
+      ),
+    ),
+
+  __record_query_options: ($) =>
+    choice(
+      seq(alias($.__record_query_of_phrase, $.of_phrase), optional($.__record_query_after_of)),
+      seq(
+        alias($.__record_query_using_phrase, $.using_phrase),
+        optional($.__record_query_after_using),
+      ),
+      seq(
+        alias($.__record_query_frame_phrase, $.frame_phrase),
+        optional($.__record_query_after_frame),
+      ),
+      $.__record_query_where_or_lock,
+      $.__record_query_use_index,
+    ),
+  __record_query_after_of: ($) =>
+    choice(
+      seq(
+        alias($.__record_query_using_phrase, $.using_phrase),
+        optional($.__record_query_after_using),
+      ),
+      seq(
+        alias($.__record_query_frame_phrase, $.frame_phrase),
+        optional($.__record_query_after_frame),
+      ),
+      $.__record_query_where_or_lock,
+      $.__record_query_use_index,
+    ),
+  __record_query_after_using: ($) =>
+    choice(
+      seq(
+        alias($.__record_query_frame_phrase, $.frame_phrase),
+        optional($.__record_query_after_frame),
+      ),
+      $.__record_query_where_or_lock,
+      $.__record_query_use_index,
+    ),
+  __record_query_after_frame: ($) =>
+    choice($.__record_query_where_or_lock, $.__record_query_use_index),
+  __record_query_where_or_lock: ($) =>
+    choice(
+      seq(
+        alias($.__record_query_where_phrase, $.where_phrase),
+        optional($.__record_query_after_where),
+      ),
+      seq(alias($.__record_query_lock_phrase, $.no_lock), optional($.__record_query_after_lock)),
+    ),
+  __record_query_after_where: ($) =>
+    choice(
+      seq(alias($.__record_query_lock_phrase, $.no_lock), optional($.__record_query_use_index)),
+      $.__record_query_use_index,
+    ),
+  __record_query_after_lock: ($) =>
+    choice(
+      seq(
+        alias($.__record_query_where_phrase, $.where_phrase),
         optional($.__record_query_use_index),
       ),
+      $.__record_query_use_index,
     ),
 
   __record_query_where_phrase: ($) => seq(kw("WHERE"), optional($._expression)),
