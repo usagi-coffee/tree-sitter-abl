728e2401768ea36c383640504e1792f71d4e0a9b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-13 00:32:56 +0200

    grammar: collapse slider option tail
    ACTION_COUNT: 42814 -> 42778 (-36)
    STATE_COUNT: 33185 -> 33127 (-58)
    LARGE_STATE_COUNT: 14824 -> 14824 (0)
    PARSER_C_SIZE_BYTES: 121223441 -> 121123843 (-99598)
    PARSER_SPEED_BYTES_MS: 60.09

--- grammar/phrases/slider.js
+++ grammar/phrases/slider.js
@@ -6,13 +6,30 @@ module.exports = ({ kw }) => ({
       field("max", $.number_literal),
       kw("MIN-VALUE"),
       field("min", $.number_literal),
-      optional(choice(kw("HORIZONTAL"), kw("VERTICAL"))),
-      optional(kw("NO-CURRENT-VALUE")),
-      optional(kw("LARGE-TO-SMALL")),
-      optional($.__slider_tic_marks),
-      optional($._tooltip_phrase),
-      optional($.size_phrase),
+      optional($.__slider_options),
     ),
+  __slider_options: ($) =>
+    choice(
+      seq(choice(kw("HORIZONTAL"), kw("VERTICAL")), optional($.__slider_after_orientation)),
+      $.__slider_after_orientation,
+    ),
+  __slider_after_orientation: ($) =>
+    choice(
+      seq(kw("NO-CURRENT-VALUE"), optional($.__slider_after_current_value)),
+      $.__slider_after_current_value,
+    ),
+  __slider_after_current_value: ($) =>
+    choice(
+      seq(kw("LARGE-TO-SMALL"), optional($.__slider_after_direction)),
+      $.__slider_after_direction,
+    ),
+  __slider_after_direction: ($) =>
+    choice(
+      seq($.__slider_tic_marks, optional($.__slider_after_tic_marks)),
+      $.__slider_after_tic_marks,
+    ),
+  __slider_after_tic_marks: ($) =>
+    choice(seq($._tooltip_phrase, optional($.size_phrase)), $.size_phrase),
 
   __slider_tic_marks: ($) =>
     seq(
