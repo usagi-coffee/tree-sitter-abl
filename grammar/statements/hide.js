1ec26311af972ce0aa836fa7abfcbb8c432e67fe
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-15 10:31:49 +0200

    grammar: factor hide stream continuation
    ACTION_COUNT: 38386 -> 38386 (0)
    STATE_COUNT: 30516 -> 30515 (-1)
    LARGE_STATE_COUNT: 13956 -> 13956 (0)
    PARSER_C_SIZE_BYTES: 114515763 -> 114515483 (-280)
    PARSER_SPEED_BYTES_MS: 79.00

--- grammar/statements/hide.js
+++ grammar/statements/hide.js
@@ -2,16 +2,9 @@ export default ({ kw }) => ({
   hide_statement: ($) => seq($.__hide_prefix, $._terminator),
   __hide_prefix: ($) => seq(kw("HIDE"), optional($.__hide_tail)),
   __hide_tail: ($) =>
-    choice(
-      seq(
-        $._stream_phrase,
-        optional(
-          choice(seq($.__hide_target, optional($.__hide_window_tail)), $.__hide_window_tail),
-        ),
-      ),
-      seq($.__hide_target, optional($.__hide_window_tail)),
-      $.__hide_window_tail,
-    ),
+    choice(seq($._stream_phrase, optional($.__hide_after_stream)), $.__hide_after_stream),
+  __hide_after_stream: ($) =>
+    choice(seq($.__hide_target, optional($.__hide_window_tail)), $.__hide_window_tail),
   __hide_window_tail: ($) =>
     choice(
       seq(alias(kw("NO-PAUSE"), $.no_pause), optional($.in_window_phrase)),
