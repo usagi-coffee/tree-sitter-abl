8714b78261204d23983adda43a7e732c0b37ea00
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-15 05:49:12 +0200

    grammar: extract conditional expression head
    ACTION_COUNT: 38332 -> 38336 (+4)
    STATE_COUNT: 30853 -> 30580 (-273)
    LARGE_STATE_COUNT: 14081 -> 13944 (-137)
    PARSER_C_SIZE_BYTES: 115216032 -> 114392883 (-823149)
    PARSER_SPEED_BYTES_MS: 80.67

--- grammar/expressions/conditional.js
+++ grammar/expressions/conditional.js
@@ -1,14 +1,8 @@
 export default ({ kw }) => ({
   conditional_expression: ($) =>
-    seq(
-      $.__conditional_prefix,
-      $._expression,
-      $.__conditional_then,
-      $._expression,
-      $.__conditional_else,
-      $._expression,
-    ),
+    seq($.__conditional_head, $._expression, $.__conditional_else, $._expression),
 
+  __conditional_head: ($) => seq($.__conditional_prefix, $._expression, $.__conditional_then),
   __conditional_prefix: ($) => kw("IF"),
   __conditional_then: ($) => kw("THEN"),
   __conditional_else: ($) => kw("ELSE"),
