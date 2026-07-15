ad4d528d5febf195fb67b2abe52ddfb4388de9dd
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-15 05:53:04 +0200

    grammar: extract conditional expression middle
    ACTION_COUNT: 38336 -> 38340 (+4)
    STATE_COUNT: 30580 -> 30444 (-136)
    LARGE_STATE_COUNT: 13944 -> 13944 (0)
    PARSER_C_SIZE_BYTES: 114392883 -> 114195476 (-197407)
    PARSER_SPEED_BYTES_MS: 81.00

--- grammar/expressions/conditional.js
+++ grammar/expressions/conditional.js
@@ -1,8 +1,8 @@
 export default ({ kw }) => ({
-  conditional_expression: ($) =>
-    seq($.__conditional_head, $._expression, $.__conditional_else, $._expression),
+  conditional_expression: ($) => seq($.__conditional_head, $.__conditional_middle, $._expression),
 
   __conditional_head: ($) => seq($.__conditional_prefix, $._expression, $.__conditional_then),
+  __conditional_middle: ($) => seq($._expression, $.__conditional_else),
   __conditional_prefix: ($) => kw("IF"),
   __conditional_then: ($) => kw("THEN"),
   __conditional_else: ($) => kw("ELSE"),
