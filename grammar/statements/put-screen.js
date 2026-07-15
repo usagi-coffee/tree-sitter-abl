a239dfec8142be5ba3959b174df22d709701d9af
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-15 08:53:29 +0200

    grammar: extract put screen output
    ACTION_COUNT: 38543 -> 38519 (-24)
    STATE_COUNT: 30651 -> 30631 (-20)
    LARGE_STATE_COUNT: 13998 -> 13985 (-13)
    PARSER_C_SIZE_BYTES: 114916581 -> 114823380 (-93201)
    PARSER_SPEED_BYTES_MS: 79.33

--- grammar/statements/put-screen.js
+++ grammar/statements/put-screen.js
@@ -5,19 +5,21 @@ export default ({ kw }) => ({
     seq(
       kw("SCREEN"),
       optional(alias($.__put_screen_color_phrase, $.color_phrase)),
-      choice(
-        // Standard order: [ROW] [COL] value.
-        seq(
-          optional(seq(kw("ROW"), field("row", $._expression))),
-          optional(seq(choice(kw("COLUMN"), kw("COL")), field("column", $._expression))),
-          field("value", $._expression),
-        ),
-        // Legacy order: value ROW [COL]. ROW is required to disambiguate.
-        seq(
-          field("value", $._expression),
-          seq(kw("ROW"), field("row", $._expression)),
-          optional(seq(choice(kw("COLUMN"), kw("COL")), field("column", $._expression))),
-        ),
+      $.__put_screen_output,
+    ),
+  __put_screen_output: ($) =>
+    choice(
+      // Standard order: [ROW] [COL] value.
+      seq(
+        optional(seq(kw("ROW"), field("row", $._expression))),
+        optional(seq(choice(kw("COLUMN"), kw("COL")), field("column", $._expression))),
+        field("value", $._expression),
+      ),
+      // Legacy order: value ROW [COL]. ROW is required to disambiguate.
+      seq(
+        field("value", $._expression),
+        seq(kw("ROW"), field("row", $._expression)),
+        optional(seq(choice(kw("COLUMN"), kw("COL")), field("column", $._expression))),
       ),
     ),
 
