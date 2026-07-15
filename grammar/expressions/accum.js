aa3d4642031c2529af7ff8d5dcb2e7c768860c6c
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-15 05:39:31 +0200

    grammar: inline accum aggregate phrase
    ACTION_COUNT: 38336 -> 38332 (-4)
    STATE_COUNT: 30991 -> 30990 (-1)
    LARGE_STATE_COUNT: 14081 -> 14081 (0)
    PARSER_C_SIZE_BYTES: 115358681 -> 115348907 (-9774)
    PARSER_SPEED_BYTES_MS: 80.00

--- grammar/expressions/accum.js
+++ grammar/expressions/accum.js
@@ -2,11 +2,10 @@ export default ({ kw }) => ({
   accum_expression: ($) =>
     seq(
       $.__accum_prefix,
-      field("operation", $.__accum_aggregate),
+      field("operation", $.aggregate_phrase),
       field("field", $.__accum_expression),
     ),
 
   __accum_prefix: ($) => kw("ACCUM"),
-  __accum_aggregate: ($) => $.aggregate_phrase,
   __accum_expression: ($) => $._expression,
 });
