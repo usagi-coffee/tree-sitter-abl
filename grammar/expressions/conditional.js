37f2c5fe718e6c39840bac679030aef743a59d03
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-31 14:42:31 +0200

    grammar: inline conditional expression prefix
    ACTION_COUNT: 38172 -> 38168 (-4)
    STATE_COUNT: 30430 -> 30429 (-1)
    LARGE_STATE_COUNT: 13948 -> 13948 (0)
    PARSER_C_SIZE_BYTES: 114363898 -> 114218664 (-145234)

--- grammar/expressions/conditional.js
+++ grammar/expressions/conditional.js
@@ -1,9 +1,8 @@
 export default ({ kw }) => ({
   conditional_expression: ($) => seq($.__conditional_head, $.__conditional_middle, $._expression),
 
-  __conditional_head: ($) => seq($.__conditional_prefix, $._expression, $.__conditional_then),
+  __conditional_head: ($) => seq(kw("IF"), $._expression, $.__conditional_then),
   __conditional_middle: ($) => seq($._expression, $.__conditional_else),
-  __conditional_prefix: ($) => kw("IF"),
   __conditional_then: ($) => kw("THEN"),
   __conditional_else: ($) => kw("ELSE"),
 });
