58035149b580e2c3e175161463ef4961eccb3e33
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-07-31 14:45:19 +0200

    grammar: inline conditional else keyword
    ACTION_COUNT: 38164 -> 38160 (-4)
    STATE_COUNT: 30428 -> 30427 (-1)
    LARGE_STATE_COUNT: 13948 -> 13948 (0)
    PARSER_C_SIZE_BYTES: 114215859 -> 114213058 (-2801)

--- grammar/expressions/conditional.js
+++ grammar/expressions/conditional.js
@@ -2,6 +2,5 @@ export default ({ kw }) => ({
   conditional_expression: ($) => seq($.__conditional_head, $.__conditional_middle, $._expression),
 
   __conditional_head: ($) => seq(kw("IF"), $._expression, kw("THEN")),
-  __conditional_middle: ($) => seq($._expression, $.__conditional_else),
-  __conditional_else: ($) => kw("ELSE"),
+  __conditional_middle: ($) => seq($._expression, kw("ELSE")),
 });
