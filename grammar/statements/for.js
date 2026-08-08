bba510ace03e79fc0476ba704f458e555d1d4d84
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-08 06:42:53 +0200

    grammar: share for by items
    ACTION_COUNT: 38198 -> 38194 (-4)
    STATE_COUNT: 30320 -> 30319 (-1)
    LARGE_STATE_COUNT: 13892 -> 13892 (0)
    PARSER_C_SIZE_BYTES: 113598554 -> 113596203 (-2351)

--- grammar/statements/for.js
+++ grammar/statements/for.js
@@ -38,20 +38,9 @@ export default ({ kw }) => ({
 
   __for_with_stream_io_phrase: ($) => seq(kw("WITH"), alias(kw("STREAM-IO"), $.stream_io)),
 
-  __for_by_tail: ($) =>
-    prec.right(
-      seq(
-        field("by", $._expression),
-        optional(field("sort_order", kw("DESCENDING", { offset: 4 }))),
-        repeat(
-          seq(
-            kw("BY"),
-            field("by", $._expression),
-            optional(field("sort_order", kw("DESCENDING", { offset: 4 }))),
-          ),
-        ),
-      ),
-    ),
+  __for_by_tail: ($) => prec.right(seq($.__for_by_item, repeat(seq(kw("BY"), $.__for_by_item)))),
+  __for_by_item: ($) =>
+    seq(field("by", $._expression), optional(field("sort_order", kw("DESCENDING", { offset: 4 })))),
 
   __for_collate_phrase: ($) =>
     seq($._collate_body, optional(field("sort_order", kw("DESCENDING", { offset: 4 })))),
