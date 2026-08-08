b04c2f59ed034f3b597a64bedabc8252bda93726
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-09 01:19:48 +0200

    grammar: factor view-as size tooltip tail
    ACTION_COUNT: 38077 -> 38079 (+2)
    STATE_COUNT: 29860 -> 29858 (-2)
    LARGE_STATE_COUNT: 13744 -> 13744 (0)
    PARSER_C_SIZE_BYTES: 112476847 -> 112474350 (-2497)

--- grammar/phrases/view-as.js
+++ grammar/phrases/view-as.js
@@ -8,19 +8,16 @@ export default ({ kw }) => ({
         seq(
           field("widget", kw("FILL-IN")),
           optional(kw("NATIVE")),
-          optional($.size_phrase),
-          optional($._tooltip_phrase),
+          optional($.__view_as_size_tooltip_tail),
         ),
         $.radio_set_phrase,
         $.selection_list_phrase,
         $.slider_phrase,
-        seq(field("widget", kw("TEXT")), optional($.size_phrase), optional($._tooltip_phrase)),
-        seq(
-          field("widget", kw("TOGGLE-BOX")),
-          optional($.size_phrase),
-          optional($._tooltip_phrase),
-        ),
+        seq(field("widget", kw("TEXT")), optional($.__view_as_size_tooltip_tail)),
+        seq(field("widget", kw("TOGGLE-BOX")), optional($.__view_as_size_tooltip_tail)),
       ),
     ),
+  __view_as_size_tooltip_tail: ($) =>
+    choice(seq($.size_phrase, optional($._tooltip_phrase)), $._tooltip_phrase),
   __view_as_alert_box: ($) => seq(field("widget", kw("ALERT-BOX")), optional($._alert_box_options)),
 });
