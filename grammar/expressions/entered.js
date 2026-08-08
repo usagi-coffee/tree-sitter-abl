9c128e64d1b0a96c7521c4fda542bf3a0b65a6b3
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-08 04:28:15 +0200

    grammar: share entered precedence
    ACTION_COUNT: 38116 -> 38116 (0)
    STATE_COUNT: 30379 -> 30379 (0)
    LARGE_STATE_COUNT: 13938 -> 13938 (0)
    PARSER_C_SIZE_BYTES: 113925843 -> 113925843 (0)

--- grammar/expressions/entered.js
+++ grammar/expressions/entered.js
@@ -1,8 +1,11 @@
 export default ({ kw }) => ({
   entered_expression: ($) =>
-    choice(
-      prec.right(-2, seq(field("field", $.identifier), $.__entered_operator)),
-      prec.right(-2, seq($.__entered_operator, field("field", $.identifier))),
+    prec.right(
+      -2,
+      choice(
+        seq(field("field", $.identifier), $.__entered_operator),
+        seq($.__entered_operator, field("field", $.identifier)),
+      ),
     ),
   __entered_operator: ($) => seq(optional(alias($._not_keyword, $.not)), kw("ENTERED")),
 });
