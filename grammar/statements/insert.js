f16194fc19de3226f01620958f3fee2932e08cbc
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-09 00:54:03 +0200

    grammar: factor insert option tail
    ACTION_COUNT: 38107 -> 38105 (-2)
    STATE_COUNT: 29877 -> 29876 (-1)
    LARGE_STATE_COUNT: 13752 -> 13751 (-1)
    PARSER_C_SIZE_BYTES: 112533293 -> 112527214 (-6079)

--- grammar/statements/insert.js
+++ grammar/statements/insert.js
@@ -1,11 +1,12 @@
 export default ({ kw }) => ({
   insert_statement: ($) => seq($.__insert_prefix, $._no_error_terminator),
   __insert_prefix: ($) =>
-    seq(
-      kw("INSERT"),
-      field("record", $._identifier_or_qualified_name),
-      optional($._except_fields),
-      optional(seq(kw("USING"), field("using", $._expression))),
-      optional($.frame_phrase),
+    seq(kw("INSERT"), field("record", $._identifier_or_qualified_name), optional($.__insert_tail)),
+  __insert_tail: ($) =>
+    choice(seq($._except_fields, optional($.__insert_after_except)), $.__insert_after_except),
+  __insert_after_except: ($) =>
+    choice(
+      seq(kw("USING"), field("using", $._expression), optional($.frame_phrase)),
+      $.frame_phrase,
     ),
 });
