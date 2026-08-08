4fd577582a6ba169be6b798ef53c8704933348c1
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-08 04:14:51 +0200

    grammar: share new expression prefix
    ACTION_COUNT: 38120 -> 38120 (0)
    STATE_COUNT: 30380 -> 30380 (0)
    LARGE_STATE_COUNT: 13938 -> 13938 (0)
    PARSER_C_SIZE_BYTES: 113935424 -> 113935424 (0)

--- grammar/expressions/new.js
+++ grammar/expressions/new.js
@@ -1,16 +1,15 @@
 export default ({ kw }) => ({
   new_expression: ($) =>
-    choice(
-      // Class constructor: NEW ClassName(args)
-      seq(
-        $.__new_prefix,
-        field("type", choice($.scoped_name, $._identifier_or_qualified_name, $.string_literal)),
-        $.arguments,
-      ),
-      // Record buffer check: NEW record or NEW(record)
-      prec.right(
+    seq(
+      $.__new_prefix,
+      choice(
+        // Class constructor: NEW ClassName(args)
         seq(
-          $.__new_prefix,
+          field("type", choice($.scoped_name, $._identifier_or_qualified_name, $.string_literal)),
+          $.arguments,
+        ),
+        // Record buffer check: NEW record or NEW(record)
+        prec.right(
           choice(
             field("record", $._identifier_or_qualified_name),
             seq($.__new_record_opener, field("record", $._identifier_or_qualified_name), ")"),
