96d171a1f6104422753edcc8ab6973d67cfa428a
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-09 00:57:14 +0200

    grammar: factor load option tail
    ACTION_COUNT: 38105 -> 38075 (-30)
    STATE_COUNT: 29876 -> 29864 (-12)
    LARGE_STATE_COUNT: 13751 -> 13744 (-7)
    PARSER_C_SIZE_BYTES: 112527214 -> 112478395 (-48819)

--- grammar/statements/load.js
+++ grammar/statements/load.js
@@ -1,13 +1,20 @@
 export default ({ kw }) => ({
   load_statement: ($) => seq($.__load_prefix, $._no_error_terminator),
 
-  __load_prefix: ($) =>
-    seq(
-      kw("LOAD"),
-      field("file", $._expression),
-      optional(seq(kw("DIR"), field("dir", $._expression))),
-      optional(seq(kw("APPLICATION"), field("app", $._expression))),
-      optional(alias(kw("NEW"), $.new)),
-      optional(seq(kw("BASE-KEY"), field("base_key", $._expression))),
+  __load_prefix: ($) => seq(kw("LOAD"), field("file", $._expression), optional($.__load_tail)),
+  __load_tail: ($) =>
+    choice(
+      seq(kw("DIR"), field("dir", $._expression), optional($.__load_after_dir)),
+      $.__load_after_dir,
+    ),
+  __load_after_dir: ($) =>
+    choice(
+      seq(kw("APPLICATION"), field("app", $._expression), optional($.__load_after_application)),
+      $.__load_after_application,
+    ),
+  __load_after_application: ($) =>
+    choice(
+      seq(alias(kw("NEW"), $.new), optional(seq(kw("BASE-KEY"), field("base_key", $._expression)))),
+      seq(kw("BASE-KEY"), field("base_key", $._expression)),
     ),
 });
