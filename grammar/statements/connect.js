5644d82286fbe1b29b7f25fcc6916a2dfe437014
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-09 16:30:27 +0200

    grammar: normalize connect options
    ACTION_COUNT: 37755 -> 37753 (-2)
    STATE_COUNT: 29586 -> 29585 (-1)
    LARGE_STATE_COUNT: 13627 -> 13627 (0)
    PARSER_C_SIZE_BYTES: 110869532 -> 110869208 (-324)

--- grammar/statements/connect.js
+++ grammar/statements/connect.js
@@ -1,19 +1,10 @@
 export default ({ kw }) => ({
   connect_statement: ($) => seq($.__connect_prefix, $._no_error_terminator),
-  __connect_prefix: ($) => seq(kw("CONNECT"), optional($.__connect_tail)),
-  __connect_tail: ($) =>
-    choice(
-      seq(
-        field("database", choice($.identifier, $.string_literal, $.function_call)),
-        repeat(
-          seq(
-            "-",
-            field("parameter", alias(token.immediate(/[\p{L}\p{N}_]+/i), $.identifier)),
-            optional(field("value", choice($.identifier, $.string_literal, $.number_literal))),
-          ),
-        ),
-      ),
-      repeat1(
+  __connect_prefix: ($) =>
+    seq(
+      kw("CONNECT"),
+      optional(field("database", choice($.identifier, $.string_literal, $.function_call))),
+      repeat(
         seq(
           "-",
           field("parameter", alias(token.immediate(/[\p{L}\p{N}_]+/i), $.identifier)),
