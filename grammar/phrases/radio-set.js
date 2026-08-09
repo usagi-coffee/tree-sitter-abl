d5a787d00c4eb0ac1a49727612d2ea2188ec35ae
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-09 02:55:37 +0200

    grammar: factor radio-set orientation size prefix
    ACTION_COUNT: 37919 -> 37915 (-4)
    STATE_COUNT: 29762 -> 29756 (-6)
    LARGE_STATE_COUNT: 13709 -> 13707 (-2)
    PARSER_C_SIZE_BYTES: 111827329 -> 111812027 (-15302)

--- grammar/phrases/radio-set.js
+++ grammar/phrases/radio-set.js
@@ -2,13 +2,14 @@ export default ({ kw }) => ({
   radio_set_phrase: ($) =>
     seq(
       kw("RADIO-SET"),
-      optional($.__radio_set_orientation),
-      optional($.size_phrase),
+      optional($.__radio_set_orientation_size_prefix),
       kw("RADIO-BUTTONS"),
       field("buttons", $.__radio_set_button_list),
       optional($.__radio_set_size_tooltip_tail),
     ),
 
+  __radio_set_orientation_size_prefix: ($) =>
+    choice(seq($.__radio_set_orientation, optional($.size_phrase)), $.size_phrase),
   __radio_set_size_tooltip_tail: ($) =>
     choice(seq($.size_phrase, optional($._tooltip_phrase)), $._tooltip_phrase),
   __radio_set_orientation: ($) =>
