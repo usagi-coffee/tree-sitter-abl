57bdc0f79bd6eb5967ea80399faa5c975cf6da4b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-09 18:20:53 +0200

    grammar: flatten hide stream tail
    ACTION_COUNT: 37729 -> 37727 (-2)
    STATE_COUNT: 29580 -> 29579 (-1)
    LARGE_STATE_COUNT: 13627 -> 13627 (0)
    PARSER_C_SIZE_BYTES: 110855326 -> 110852819 (-2507)

--- grammar/statements/hide.js
+++ grammar/statements/hide.js
@@ -1,8 +1,7 @@
 export default ({ kw }) => ({
   hide_statement: ($) => seq($.__hide_prefix, $._terminator),
-  __hide_prefix: ($) => seq(kw("HIDE"), optional($.__hide_tail)),
-  __hide_tail: ($) =>
-    choice(seq($._stream_phrase, optional($.__hide_after_stream)), $.__hide_after_stream),
+  __hide_prefix: ($) =>
+    seq(kw("HIDE"), optional($._stream_phrase), optional($.__hide_after_stream)),
   __hide_after_stream: ($) =>
     choice(seq($.__hide_target, optional($.__hide_window_tail)), $.__hide_window_tail),
   __hide_window_tail: ($) =>
