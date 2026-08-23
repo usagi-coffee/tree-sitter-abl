cfc97452392542d16fc57bf878826f92f7533bce
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-23 02:22:03 +0200

    grammar: extract expression statement body
    ACTION_COUNT: 50149 -> 50149 (0)
    STATE_COUNT: 38950 -> 38942 (-8)
    LARGE_STATE_COUNT: 18881 -> 18877 (-4)
    PARSER_C_SIZE_BYTES: 148311109 -> 148268925 (-42184)

--- grammar/statements/expression.js
+++ grammar/statements/expression.js
@@ -1,9 +1,7 @@
 export default ({ kw }) => ({
-  expression_statement: ($) =>
-    seq(
-      choice($._statement_expression, alias($.__expression_keyword_call, $.function_call)),
-      $._no_error_terminator,
-    ),
+  expression_statement: ($) => seq($.__expression_statement_body, $._no_error_terminator),
+  __expression_statement_body: ($) =>
+    choice($._statement_expression, alias($.__expression_keyword_call, $.function_call)),
 
   __expression_keyword_call: ($) =>
     seq(field("function", alias(kw("CLOSE"), $.identifier)), $.arguments),
