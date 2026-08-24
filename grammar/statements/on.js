743b423ce653091b4e69261e919409a94cf0eec3
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-24 02:32:01 +0200

    grammar: recurse ON widget list tail
    Replace the repeated comma-separated ON widget sequence with a recursive tail while preserving the of_phrase tree shape.
    
    The simpler repeated sequence was measured and produced a more expensive parser.
    
    ACTION_COUNT: 44528 -> 44525 (-3)
    STATE_COUNT: 34808 -> 34808 (0)
    LARGE_STATE_COUNT: 16770 -> 16770 (0)
    PARSER_C_SIZE_BYTES: 131976470 -> 131976094 (-376)

--- grammar/statements/on.js
+++ grammar/statements/on.js
@@ -247,7 +247,8 @@ export default ({ kw }) => ({
   __on_ui_events: ($) =>
     seq(field("event", $.__on_ui_event), repeat(seq(",", field("event", $.__on_ui_event)))),
   __on_ui_event_widgets: ($) => seq($.__on_ui_events, alias($.__on_of_phrase, $.of_phrase)),
-  __on_of_phrase: ($) => seq(kw("OF"), $.__on_of_widget, repeat(seq(",", $.__on_of_widget))),
+  __on_of_phrase: ($) => seq(kw("OF"), $.__on_of_widget, optional($.__on_of_widget_tail)),
+  __on_of_widget_tail: ($) => seq(",", $.__on_of_widget, optional($.__on_of_widget_tail)),
   __on_of_widget: ($) => seq(field("widget", $.widget_phrase)),
   __on_referencing_phrase: ($) =>
     choice(
