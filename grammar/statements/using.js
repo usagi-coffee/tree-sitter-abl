b83fe543500b715dc25280d4257ee572711f44db
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 01:12:03 +0200

    grammar: recurse using generic arguments
    Parse comma-separated USING generic type arguments through a recursive tail while preserving nested type syntax.
    
    ACTION_COUNT: 43180 -> 43173 (-7)
    STATE_COUNT: 34901 -> 34897 (-4)
    LARGE_STATE_COUNT: 16742 -> 16742 (0)
    PARSER_C_SIZE_BYTES: 130517335 -> 130515300 (-2035)

--- grammar/statements/using.js
+++ grammar/statements/using.js
@@ -13,12 +13,7 @@ export default ({ kw }) => ({
 
   __using_type_ref: ($) =>
     choice(
-      prec.right(
-        seq(
-          $.__using_base_name,
-          optional(seq("<", $.__using_type_ref, repeat(seq(",", $.__using_type_ref)), ">")),
-        ),
-      ),
+      prec.right(seq($.__using_base_name, optional(seq("<", $.__using_type_arguments, ">")))),
       $.string_literal,
     ),
 
@@ -30,6 +25,9 @@ export default ({ kw }) => ({
         optional(seq(".", "*")),
       ),
     ),
+
+  __using_type_arguments: ($) =>
+    prec.right(seq($.__using_type_ref, optional(seq(",", $.__using_type_arguments)))),
 
   __using_from_clause: ($) => seq(kw("FROM"), choice(kw("ASSEMBLY"), kw("PROPATH"))),
 });
