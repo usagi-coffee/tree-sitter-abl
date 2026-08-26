948dc7c022d10195d8ec7a8ba9470f920c495c6b
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 14:25:03 +0200

    grammar: recurse preselect record list
    Replace the repeated PRESELECT record tail with a hidden recursive sequence while preserving comma ordering and record nodes.
    
    ACTION_COUNT: 40031 -> 40026 (-5)
    STATE_COUNT: 34806 -> 34806 (+0)
    LARGE_STATE_COUNT: 16695 -> 16695 (+0)
    PARSER_C_SIZE_BYTES: 129657916 -> 129657039 (-877)

--- grammar/statements/preselect.js
+++ grammar/statements/preselect.js
@@ -3,7 +3,9 @@ export default ({ kw }) => ({
 
   __preselect_prefix: ($) => seq(kw("PRESELECT"), $.preselect_record_list),
 
-  preselect_record_list: ($) => seq($.preselect_record, repeat(seq(",", $.preselect_record))),
+  preselect_record_list: ($) => seq($.preselect_record, optional($.__preselect_record_list_tail)),
+  __preselect_record_list_tail: ($) =>
+    prec.right(seq(",", $.preselect_record, optional($.__preselect_record_list_tail))),
 
   preselect_record: ($) =>
     seq(optional(choice(kw("EACH"), kw("FIRST"), kw("LAST"))), $.record_phrase),
