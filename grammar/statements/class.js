df29e3dd70fd2ff53e8524a11e216ad0965a641a
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 12:52:39 +0200

    grammar: recurse class property options
    Replace repeated class-property options with a hidden recursive tail while preserving each option alias and field.
    
    ACTION_COUNT: 40127 -> 40117 (-10)
    STATE_COUNT: 34800 -> 34801 (+1)
    LARGE_STATE_COUNT: 16695 -> 16695 (+0)
    PARSER_C_SIZE_BYTES: 129756841 -> 129747489 (-9352)

--- grammar/statements/class.js
+++ grammar/statements/class.js
@@ -185,14 +185,17 @@ export default ({ kw }) => ({
       kw("PROPERTY"),
       field("name", $.identifier),
       $.__class_property_type_phrase,
-      repeat(
-        choice(
-          seq(kw("INITIAL", { offset: 4 }), field("initial", $._initial_value)),
-          seq(kw("SERIALIZE-NAME"), field("serialize_name", $.string_literal)),
-          alias($._no_undo_keyword, $.no_undo),
-          alias($._extent_phrase, $.extent_phrase),
-        ),
-      ),
+      optional($.__class_property_options),
+    ),
+
+  __class_property_options: ($) =>
+    prec.right(seq($.__class_property_option, optional($.__class_property_options))),
+  __class_property_option: ($) =>
+    choice(
+      seq(kw("INITIAL", { offset: 4 }), field("initial", $._initial_value)),
+      seq(kw("SERIALIZE-NAME"), field("serialize_name", $.string_literal)),
+      alias($._no_undo_keyword, $.no_undo),
+      alias($._extent_phrase, $.extent_phrase),
     ),
 
   __class_property_accessor_body: ($) =>
