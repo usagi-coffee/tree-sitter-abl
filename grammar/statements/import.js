b08cdfc2f1916a1e673def7baea324c064685bc4
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 04:41:34 +0200

    grammar: recurse import fields
    Replace the repeated IMPORT field choice with a recursive helper while preserving field and skip-field order.
    
    ACTION_COUNT: 41254 -> 41232 (-22)
    
    STATE_COUNT: 34870 -> 34871 (+1)
    
    LARGE_STATE_COUNT: 16721 -> 16721 (0)
    
    PARSER_C_SIZE_BYTES: 130212897 -> 130212858 (-39)

--- grammar/statements/import.js
+++ grammar/statements/import.js
@@ -12,11 +12,18 @@ export default ({ kw }) => ({
   __import_fields_phrase: ($) =>
     seq(
       optional(alias($.__import_delimiter_phrase, $.delimiter_phrase)),
-      repeat1(choice($._identifier_or_qualified_name, alias("^", $.skip_field))),
+      $.__import_fields,
       optional(seq(kw("EXCEPT"), $.__import_except_names)),
     ),
   __import_except_names: ($) =>
     prec.right(seq($._identifier_or_qualified_name, optional($.__import_except_names))),
+  __import_fields: ($) =>
+    prec.right(
+      seq(
+        choice($._identifier_or_qualified_name, alias("^", $.skip_field)),
+        optional($.__import_fields),
+      ),
+    ),
   __import_unformatted_phrase: ($) =>
     seq(kw("UNFORMATTED"), field("field", $._identifier_or_qualified_name)),
 });
