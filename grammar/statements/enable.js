44a6730aab9eb2b7db7648163b397e99349c7ef1
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 03:17:56 +0200

    grammar: recurse enable items
    Parse required ENABLE items through a recursive tail while preserving enable_item aliases and ALL handling.
    
    ACTION_COUNT: 42465 -> 42419 (-46)
    STATE_COUNT: 34869 -> 34869 (0)
    LARGE_STATE_COUNT: 16739 -> 16739 (0)
    PARSER_C_SIZE_BYTES: 130404712 -> 130401050 (-3662)

--- grammar/statements/enable.js
+++ grammar/statements/enable.js
@@ -15,8 +15,11 @@ export default ({ kw }) => ({
   __enable_body: ($) =>
     choice(
       seq(alias(kw("ALL"), $.all), optional(seq(kw("EXCEPT"), $._except_name_list))),
-      repeat1(alias($.__enable_item, $.enable_item)),
+      $.__enable_items_list,
     ),
+
+  __enable_items_list: ($) =>
+    prec.right(seq(alias($.__enable_item, $.enable_item), optional($.__enable_items_list))),
 
   __enable_item: ($) =>
     choice(
