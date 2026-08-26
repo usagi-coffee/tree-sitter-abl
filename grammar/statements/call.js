b8b8fbb9bc4f77bb9eabd9dc82185d7f77a20ef0
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 05:01:47 +0200

    grammar: recurse call arguments
    Replace the repeated CALL argument aliases with a recursive helper while preserving argument nodes and ordering.
    
    ACTION_COUNT: 41136 -> 41050 (-86)
    
    STATE_COUNT: 34871 -> 34871 (0)
    
    LARGE_STATE_COUNT: 16719 -> 16718 (-1)
    
    PARSER_C_SIZE_BYTES: 130192817 -> 130180663 (-12154)

--- grammar/statements/call.js
+++ grammar/statements/call.js
@@ -2,7 +2,10 @@ export default ({ kw }) => ({
   call_statement: ($) => seq($.__call_prefix, $._terminator),
 
   __call_prefix: ($) =>
-    seq(kw("CALL"), field("routine", $.identifier), repeat(alias($.__call_argument, $.argument))),
+    seq(kw("CALL"), field("routine", $.identifier), optional($.__call_arguments)),
+
+  __call_arguments: ($) =>
+    prec.right(seq(alias($.__call_argument, $.argument), optional($.__call_arguments))),
 
   __call_argument: ($) => $._expression,
 });
