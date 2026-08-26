8736765905a2b5a845ebca533ea7294662803812
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 02:23:08 +0200

    grammar: recurse os-delete targets
    Parse required OS-DELETE targets through a recursive tail while preserving VALUE operands and RECURSIVE placement.
    
    ACTION_COUNT: 42841 -> 42800 (-41)
    STATE_COUNT: 34876 -> 34876 (0)
    LARGE_STATE_COUNT: 16742 -> 16741 (-1)
    PARSER_C_SIZE_BYTES: 130459703 -> 130450844 (-8859)

--- grammar/statements/os-delete.js
+++ grammar/statements/os-delete.js
@@ -2,12 +2,10 @@ export default ({ kw }) => ({
   os_delete_statement: ($) => seq($.__os_delete_prefix, $._terminator),
 
   __os_delete_prefix: ($) =>
-    seq(
-      kw("OS-DELETE"),
-      repeat1($.__os_delete_target),
-      optional(alias(kw("RECURSIVE"), $.recursive)),
-    ),
+    seq(kw("OS-DELETE"), $.__os_delete_targets, optional(alias(kw("RECURSIVE"), $.recursive))),
 
   __os_delete_target: ($) =>
     choice($._os_filename, seq(kw("VALUE"), "(", field("value", $._expression), ")")),
+  __os_delete_targets: ($) =>
+    prec.right(seq($.__os_delete_target, optional($.__os_delete_targets))),
 });
