e233d2d44465328d4fad0bb61510d2cac18a9787
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 02:27:55 +0200

    grammar: recurse view widget targets
    Parse required VIEW widget targets through a recursive tail while preserving the alternative IN WINDOW form.
    
    ACTION_COUNT: 42771 -> 42715 (-56)
    STATE_COUNT: 34875 -> 34875 (0)
    LARGE_STATE_COUNT: 16740 -> 16739 (-1)
    PARSER_C_SIZE_BYTES: 130445875 -> 130436260 (-9615)

--- grammar/statements/view.js
+++ grammar/statements/view.js
@@ -2,5 +2,6 @@ export default ({ kw }) => ({
   view_statement: ($) => seq($.__view_prefix, $._terminator),
   __view_prefix: ($) => seq(kw("VIEW"), optional($._stream_phrase), optional($.__view_window_tail)),
   __view_window_tail: ($) =>
-    choice(seq(repeat1($.widget_phrase), optional($.in_window_phrase)), $.in_window_phrase),
+    choice(seq($.__view_widgets, optional($.in_window_phrase)), $.in_window_phrase),
+  __view_widgets: ($) => prec.right(seq($.widget_phrase, optional($.__view_widgets))),
 });
