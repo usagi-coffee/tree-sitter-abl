7b4f197f51cc9ec4a0247b19bb3635de888c82fd
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 07:58:07 +0200

    grammar: recurse browse options
    Replace the split inlined BROWSE option tail with one recursive helper while preserving option order and syntax-tree output.
    
    ACTION_COUNT: 40430 -> 40428 (-2)
    STATE_COUNT: 34875 -> 34874 (-1)
    LARGE_STATE_COUNT: 16707 -> 16706 (-1)
    PARSER_C_SIZE_BYTES: 130109033 -> 130102031 (-7002)

--- grammar/statements/browse.js
+++ grammar/statements/browse.js
@@ -21,9 +21,7 @@ export default ({ kw }) => ({
       $.__browse_options_phrase,
     ),
   __browse_options_phrase: ($) => seq($._with_keyword, optional($.__browse_options)),
-  __browse_options: ($) => seq($.__browse_option, optional($.__browse_options_tail)),
-  __browse_options_tail: ($) =>
-    prec.right(seq($.__browse_option, optional($.__browse_options_tail))),
+  __browse_options: ($) => prec.right(seq($.__browse_option, optional($.__browse_options))),
   __browse_option: ($) =>
     choice(
       $._with_keyword,
