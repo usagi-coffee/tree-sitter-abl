1fb510ddd54af2a88ed8d69da7dc271dc7b943ca
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-08-26 03:40:39 +0200

    grammar: recurse accumulate items
    Parse required ACCUMULATE items through a recursive tail while preserving accumulate aliases and aggregate syntax.
    
    ACTION_COUNT: 42190 -> 42104 (-86)
    STATE_COUNT: 34869 -> 34869 (0)
    LARGE_STATE_COUNT: 16735 -> 16734 (-1)
    PARSER_C_SIZE_BYTES: 130358999 -> 130346735 (-12264)

--- grammar/statements/accumulate.js
+++ grammar/statements/accumulate.js
@@ -1,8 +1,10 @@
 export default ({ kw }) => ({
   accumulate_statement: ($) => seq($.__accumulate_prefix, $._terminator),
 
-  __accumulate_prefix: ($) =>
-    seq(kw("ACCUMULATE"), repeat1(alias($.__accumulate_item, $.accumulate))),
+  __accumulate_prefix: ($) => seq(kw("ACCUMULATE"), $.__accumulate_items),
+
+  __accumulate_items: ($) =>
+    prec.right(seq(alias($.__accumulate_item, $.accumulate), optional($.__accumulate_items))),
 
   __accumulate_item: ($) =>
     seq(field("target", $._expression), "(", repeat1($.aggregate_phrase), ")"),
