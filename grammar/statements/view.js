2385fa73b6dc66d0fae26b7f7929623e8f1ca404
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-12 21:21:43 +0200

    grammar: annotate rejected view tail optimization
    The measured non-empty VIEW stream/window tail extraction reduced parser size by 555 bytes but increased ACTION_COUNT by 2 and STATE_COUNT by 1.

--- grammar/statements/view.js
+++ grammar/statements/view.js
@@ -1,5 +1,6 @@
 export default ({ kw }) => ({
   view_statement: ($) => seq($.__view_prefix, $._terminator),
+  // oxlint-disable-next-line tree-sitter-optimize/non-empty-tail-extraction
   __view_prefix: ($) => seq(kw("VIEW"), optional($._stream_phrase), optional($.__view_window_tail)),
   __view_window_tail: ($) =>
     choice(seq($._widget_phrases, optional($.in_window_phrase)), $.in_window_phrase),
