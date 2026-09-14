834653ab840b4e6268c374591d8a069353efed33
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-14 20:34:26 +0200

    grammar: reuse the ON UI event tail
    Reuse the hidden ON UI event list after each comma while preserving event fields.
    
    ACTION_COUNT: 41796 -> 41794 (-2)
    STATE_COUNT: 34326 -> 34325 (-1)
    LARGE_STATE_COUNT: 15993 -> 15993 (0)
    PARSER_C_SIZE_BYTES: 124314604 -> 124313665 (-939)

--- grammar/statements/on.js
+++ grammar/statements/on.js
@@ -240,8 +240,7 @@ export default ({ kw }) => ({
     ),
   __on_ui_event: ($) => choice($.__on_key_label, alias(token(choice("+", "-")), $.identifier)),
   __on_ui_events: ($) => seq(field("event", $.__on_ui_event), optional($.__on_ui_events_tail)),
-  __on_ui_events_tail: ($) =>
-    seq(",", field("event", $.__on_ui_event), optional($.__on_ui_events_tail)),
+  __on_ui_events_tail: ($) => seq(",", $.__on_ui_events),
   __on_ui_event_widgets: ($) => seq($.__on_ui_events, alias($.__on_of_phrase, $.of_phrase)),
   __on_ui_event_widgets_tail: ($) =>
     seq(kw("OR"), $.__on_ui_event_widgets, optional($.__on_ui_event_widgets_tail)),
