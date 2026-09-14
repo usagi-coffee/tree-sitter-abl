10aaaac3e48651d9a8316a07583eecdf712f5730
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-14 18:13:25 +0200

    grammar: reuse the PROMSGS value sequence
    Use the shared equals/value helper after PROMSGS while preserving its value field.
    
    ACTION_COUNT: 41922 -> 41920 (-2)
    STATE_COUNT: 34404 -> 34403 (-1)
    LARGE_STATE_COUNT: 16018 -> 16017 (-1)
    PARSER_C_SIZE_BYTES: 124549690 -> 124542946 (-6744)

--- grammar/statements/promsgs.js
+++ grammar/statements/promsgs.js
@@ -1,5 +1,5 @@
 export default ({ kw }) => ({
   promsgs_statement: ($) => seq($.__promsgs_prefix, $._terminator),
 
-  __promsgs_prefix: ($) => seq(kw("PROMSGS"), "=", field("value", $._expression)),
+  __promsgs_prefix: ($) => seq(kw("PROMSGS"), $._equals_value),
 });
