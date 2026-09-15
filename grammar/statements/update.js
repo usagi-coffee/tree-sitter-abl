e9c1a46c50753439115f8290751806c368be2369
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-15 23:47:40 +0200

    grammar: retain update prefix
    Suppress the single-use choice-sequence suggestion because inlining the prefix left actions unchanged while increasing states from 34,500 to 34,510, large states from 16,387 to 16,391, and parser C size from 127,472,482 to 127,521,596 bytes.

--- grammar/statements/update.js
+++ grammar/statements/update.js
@@ -1,6 +1,7 @@
 export default ({ kw }) => ({
   update_statement: ($) => seq($.__update_prefix, $._no_error_terminator),
 
+  // oxlint-disable-next-line tree-sitter-optimize/single-use-choice-sequence
   __update_prefix: ($) => seq(kw("UPDATE"), choice($.__update_record_body, $.__update_fields_body)),
 
   __update_record_body: ($) =>
