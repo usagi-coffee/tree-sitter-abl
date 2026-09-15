31cd040a8066ddd204cedb69fc23ce779bf6cc38
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-15 23:49:57 +0200

    grammar: retain propath prefix
    Suppress the single-use choice-sequence suggestion because inlining the prefix increased actions from 41,756 to 41,774, states from 34,500 to 34,509, large states from 16,387 to 16,392, and parser C size from 127,472,482 to 127,502,925 bytes.

--- grammar/statements/propath.js
+++ grammar/statements/propath.js
@@ -1,5 +1,6 @@
 export default ({ kw }) => ({
   propath_statement: ($) => seq($.__propath_prefix, $._terminator),
 
+  // oxlint-disable-next-line tree-sitter-optimize/single-use-choice-sequence
   __propath_prefix: ($) => seq(kw("PROPATH"), choice("=", "+="), field("value", $._expression)),
 });
