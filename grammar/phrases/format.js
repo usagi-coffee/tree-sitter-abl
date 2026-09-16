6495c6161f3bac482908e0e1ad97e6ec3157ebc7
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-16 15:16:20 +0200

    grammar: inline COMBO-BOX value tail
    Inline the single-use comma continuation in the recursive COMBO-BOX value list. Value alternatives and separator order remain unchanged.
    
    ACTION_COUNT: 41584 -> 41580 (-4)
    STATE_COUNT: 34327 -> 34314 (-13)
    LARGE_STATE_COUNT: 16322 -> 16313 (-9)
    PARSER_C_SIZE_BYTES: 126975470 -> 126927204 (-48266)

--- grammar/phrases/format.js
+++ grammar/phrases/format.js
@@ -221,8 +221,7 @@ export default ({ kw }) => ({
       ),
     ),
   __format_combo_box_values: ($) =>
-    seq($.__format_radio_set_value, optional($.__format_combo_box_values_tail)),
-  __format_combo_box_values_tail: ($) => seq(",", $.__format_combo_box_values),
+    seq($.__format_radio_set_value, optional(seq(",", $.__format_combo_box_values))),
   __format_view_as_tail: ($) =>
     prec.right(choice(seq($.size_phrase, optional($._tooltip_phrase)), $._tooltip_phrase)),
   _format_view_as: ($) =>
