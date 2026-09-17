0da600b9e58d0026b133e4112061f21a1f1c8c3c
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-17 07:12:39 +0200

    grammar: shorten private DO prefix
    Rename the private DO statement prefix without changing its rule body or the exposed do_statement node.
    
    ACTION_COUNT: 41459 -> 41459 (0)
    STATE_COUNT: 34226 -> 34226 (0)
    LARGE_STATE_COUNT: 16276 -> 16276 (0)
    PARSER_C_SIZE_BYTES: 126647075 -> 126646205 (-870)

--- grammar/statements/do.js
+++ grammar/statements/do.js
@@ -1,8 +1,8 @@
 export default ({ kw }) => ({
-  do_statement: ($) => seq($.__do_statement_prefix, $._terminator),
+  do_statement: ($) => seq($.__do_prefix, $._terminator),
 
   // oxlint-disable-next-line tree-sitter-optimize/single-use-sequence
-  __do_statement_prefix: ($) => seq(optional($._label), $.__do_body, $._end_keyword),
+  __do_prefix: ($) => seq(optional($._label), $.__do_body, $._end_keyword),
 
   // Widening the FOR branch counter makes record-phrase ambiguities global.
   __do_body: ($) =>
