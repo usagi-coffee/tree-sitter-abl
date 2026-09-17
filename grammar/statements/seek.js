157f8c462a5120c5907e5ca7708f7a7c9252f250
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-17 09:31:20 +0200

    grammar: isolate the SEEK stream choice
    Move INPUT, OUTPUT and the stream phrase into a private choice so stream and destination alternatives are represented separately. Preserve all stream/target combinations.
    
    ACTION_COUNT: 41433 -> 41435 (+2)
    STATE_COUNT: 34213 -> 34211 (-2)
    LARGE_STATE_COUNT: 16275 -> 16274 (-1)
    PARSER_C_SIZE_BYTES: 126634226 -> 126628141 (-6085)

--- grammar/statements/seek.js
+++ grammar/statements/seek.js
@@ -2,12 +2,9 @@ export default ({ kw }) => ({
   seek_statement: ($) => prec.right(seq($.__seek_prefix, $._terminator)),
 
   __seek_prefix: ($) =>
-    seq(
-      kw("SEEK"),
-      choice(kw("INPUT"), kw("OUTPUT"), $._stream_phrase),
-      $._to_keyword,
-      choice($._end_keyword, $._expression),
-    ),
+    seq(kw("SEEK"), $.__seek_stream, $._to_keyword, choice($._end_keyword, $._expression)),
+
+  __seek_stream: ($) => choice(kw("INPUT"), kw("OUTPUT"), $._stream_phrase),
 
   seek_expression: ($) => seq($.__seek_expression_body, ")"),
 
