46d5e09bf943b3840579dcf0da0e4fbc037e2d09
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-24 05:59:06 +0200

    grammar: inline echo phrase choice
    Inline the ECHO and NO-ECHO alias choice into the map/unbuffered conversion tail while preserving the echo and no_echo nodes.
    
    ACTION_COUNT: 41313 -> 41311 (-2)
    STATE_COUNT: 34164 -> 34163 (-1)
    LARGE_STATE_COUNT: 16262 -> 16262 (0)
    PARSER_C_SIZE_BYTES: 126415845 -> 126414783 (-1062)

--- grammar.js
+++ grammar.js
@@ -224,6 +224,7 @@ export default grammar({
     $.__additive_operator,
     $._window_handle,
     $._block_option,
+    $._echo_phrase,
     $._primary_expression,
     $.__preprocessor_name_value,
     $._identifier_or_access_or_call,
