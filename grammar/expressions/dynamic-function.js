0a86bf2ce9da41c6a95f4279f1bef4b384f787bb
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2026-09-25 18:44:54 +0200

    grammar: suppress dynamic function inline suggestion
    Inlining reduced actions from 41442 to 41440 and held large states at 16179, but increased states from 33997 to 33998 and grew parser.c from 120743742 to 120744059 bytes. Keep the helper shared and suppress this suggestion.

--- grammar/expressions/dynamic-function.js
+++ grammar/expressions/dynamic-function.js
@@ -37,6 +37,7 @@ export default ({ kw }) => ({
     seq($.__dynamic_function_atom, $.__dynamic_function_concatenation_tail),
   __dynamic_function_concatenation_tail: ($) =>
     seq("+", $.__dynamic_function_atom, optional($.__dynamic_function_concatenation_tail)),
+  // oxlint-disable-next-line tree-sitter-optimize/multi-use-private-choice-inline
   __dynamic_function_atom: ($) =>
     choice($._string_or_identifier_access_or_call, $.parenthesized_expression),
 });
